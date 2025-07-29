7fbe3676fe38871c5b5bf0d2396a6462cec7e559
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-30 01:11:23 +0200

    Add map features

---
+++ src/modules/minigame/fire_fighters/map/map_feature.gd
@@ -0,0 +1,9 @@
+class_name FireFightersMapFeature
+extends Resource
+
+@export var atlas_coords: Vector2i
+@export var flammability: int = 0
+
+@export var spawn_noise: FastNoiseLite
+@export var spawn_noise_threshold: float = 0.0
+@export var density: float = 1.0
