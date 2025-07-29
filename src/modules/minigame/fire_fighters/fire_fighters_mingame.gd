3ab6b3e44b4038dc1034cee8aa7f3c7d6af86750
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-30 01:11:56 +0200

    Update fire_fighters_mingame.gd

--- src/modules/minigame/fire_fighters/fire_fighters_mingame.gd
+++ src/modules/minigame/fire_fighters/fire_fighters_mingame.gd
@@ -1,2 +1,28 @@
 class_name FireFightersMinigame
-#extends BaseMinigame
+extends BaseMinigame
+
+@export var player_scene: PackedScene
+
+@onready var tile_map_terrain: TileMapLayer = $"TileMapLayer Terrain"
+@onready var tile_map_objects: TileMapLayer = $"TileMapLayer Objects"
+
+
+func _ready() -> void:
+	init()
+	run()
+
+
+func init():
+	generate_map()
+
+
+func run():
+	spawn_player()
+
+
+func generate_map():
+	pass
+
+
+func spawn_player():
+	pass
