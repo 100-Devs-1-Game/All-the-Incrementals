71a991e21b0ec52f3f5c0c0f84ef66621fffecab
Nicolai Moore <niconorsk@gmail.com>
2025-07-29 19:30:04 +1000

    Fixed tree spelling and show inspector when selecting a resource

--- src/addons/upgrade_tree/upgrade_tree_editor.gd
+++ src/addons/upgrade_tree/upgrade_tree_editor.gd
@@ -4,22 +4,22 @@ extends EditorPlugin
 const UpgradeTreeEditor = preload("res://addons/upgrade_tree/upgrade_tree_editor.tscn")
 const UpgradeEditorScene = preload("res://addons/upgrade_tree/upgrade_editor.tscn")
 
-var upgrade_treee_editor_instance
+var upgrade_tree_editor_instance
 var current_scene: Node = null
 var current_selected_node: GraphNode = null
 
 
 func _enter_tree() -> void:
-	upgrade_treee_editor_instance = UpgradeTreeEditor.instantiate()
+	upgrade_tree_editor_instance = UpgradeTreeEditor.instantiate()
 	# Add the main panel to the editor's main viewport.
-	EditorInterface.get_editor_main_screen().add_child(upgrade_treee_editor_instance)
+	EditorInterface.get_editor_main_screen().add_child(upgrade_tree_editor_instance)
 
 	# Hide the main panel. Very much required.
 	_make_visible(false)
 	current_scene = EditorInterface.get_edited_scene_root()
 	scene_changed.connect(_on_scene_changed)
-	upgrade_treee_editor_instance.get_node("AddUpgrade").pressed.connect(_on_add_upgrade_pressed)
-	upgrade_treee_editor_instance.get_node("GraphEdit").connection_request.connect(
+	upgrade_tree_editor_instance.get_node("AddUpgrade").pressed.connect(_on_add_upgrade_pressed)
+	upgrade_tree_editor_instance.get_node("GraphEdit").connection_request.connect(
 		_on_connection_request
 	)
 
@@ -47,7 +47,7 @@ func _on_add_upgrade_pressed() -> void:
 		else:
 			graph_node.position_offset = Vector2(100, 100)  # Position inside the GraphEdit
 		graph_node.node_selected.connect(_on_upgrade_selected.bind(graph_node))
-		upgrade_treee_editor_instance.get_node("GraphEdit").add_child(graph_node)
+		upgrade_tree_editor_instance.get_node("GraphEdit").add_child(graph_node)
 	else:
 		push_error("No UpgradeTree scene selected.")
 
@@ -57,21 +57,22 @@ func _on_upgrade_selected(graph_node: GraphNode) -> void:
 		current_selected_node.get_node("TableContainer").hide()
 	current_selected_node = graph_node
 	graph_node.get_node("TableContainer").show()
+	EditorInterface.edit_resource(graph_node.upgrade)
 
 
 func _on_connection_request(from_node, from_port, to_node, to_port):
 	# Don't connect to input that is already connected
-	for con in upgrade_treee_editor_instance.get_node("GraphEdit").get_connection_list():
+	for con in upgrade_tree_editor_instance.get_node("GraphEdit").get_connection_list():
 		if con.to_node == to_node and con.to_port == to_port:
 			return
-	upgrade_treee_editor_instance.get_node("GraphEdit").connect_node(
+	upgrade_tree_editor_instance.get_node("GraphEdit").connect_node(
 		from_node, from_port, to_node, to_port
 	)
 
 
 func _exit_tree() -> void:
-	if upgrade_treee_editor_instance:
-		upgrade_treee_editor_instance.queue_free()
+	if upgrade_tree_editor_instance:
+		upgrade_tree_editor_instance.queue_free()
 
 
 func _handles(object: Object) -> bool:
@@ -83,11 +84,11 @@ func _has_main_screen() -> bool:
 
 
 func _make_visible(visible: bool) -> void:
-	if upgrade_treee_editor_instance:
+	if upgrade_tree_editor_instance:
 		if visible:
-			upgrade_treee_editor_instance.show()
+			upgrade_tree_editor_instance.show()
 		else:
-			upgrade_treee_editor_instance.hide()
+			upgrade_tree_editor_instance.hide()
 
 
 func _get_plugin_name():
