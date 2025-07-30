6f7ff77919310533e115e7e57fd82a3cc553806a
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-30 05:58:13 +0200

    Add signal

--- src/modules/minigame/fire_fighters/effects/fire.gd
+++ src/modules/minigame/fire_fighters/effects/fire.gd
@@ -1,12 +1,20 @@
 class_name FireFightersMinigameFire
 extends Node2D
 
+signal died
+
 @export var offset: float = 5.0
 @export var frequency: float = 10.0
 # TODO retrieve automatically
 @export var animation_frames: int = 4
 
-var size: float = 0.0
+var size: float = 0.0:
+	set(s):
+		size = s
+		if size < 0.0:
+			died.emit()
+		elif size > 1.5:
+			size = 1.5
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var orig_pos: Vector2 = position
