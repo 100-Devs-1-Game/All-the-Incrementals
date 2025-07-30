8e587d8f7b988a4dc912f10da6e9194bb6e86df2
Plant <99131806+PlantDev@users.noreply.github.com>
2025-07-30 05:23:13 -0300

    Simple Text Popup Addition (linter confuse me)
    Just adds a floating text to a given position in the game that floats up and fades away. Can be used for anything really, just need to call TextFloatSystem.floating_text(position, value, parent)

--- src/modules/minigame/common/floating_text/float_text.gd
+++ src/modules/minigame/common/floating_text/float_text.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-@export var set_font : Font
+@export var set_font: Font
 @export var float_distance := 40.0
 @export var float_duration := 0.6
+
 
 ## Displays the value and animates the floating text before freeing itself.
 func show_value(value):
 	$ValueLabel.text = str(value)
-	$ValueLabel.add_theme_font_override("font", set_font) # Likely not needed
+	$ValueLabel.add_theme_font_override("font", set_font)  # Likely not needed
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", position.y - float_distance, float_duration)
 	tween.tween_property(self, "modulate:a", 0.0, float_duration - 0.3)
--- src/modules/minigame/common/floating_text/floating_text_util.gd
+++ src/modules/minigame/common/floating_text/floating_text_util.gd
@@ -3,8 +3,9 @@ extends Node
 
 const FLOATING_TEXT_SCENE = preload("res://modules/minigame/common/floating_text/float_text.tscn")
 
+
 ## Spawns a floating text instance at a given position with the specified value, then fades away.
-static func floating_text(position : Vector2, value: String, parent: Node = null) -> void:
+static func floating_text(position: Vector2, value: String, parent: Node = null) -> void:
 	var instance = FLOATING_TEXT_SCENE.instantiate()
 	parent.add_child(instance)
 	instance.position = position
