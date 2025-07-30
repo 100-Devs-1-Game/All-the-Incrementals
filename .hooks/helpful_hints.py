1f26f4681fd85ee8c7f6eccc925a6efa084156b1
microlancer <microlancer@users.noreply.github.com>
2025-07-30 09:38:02 +0900

    Add helpful hints precommit hook

---
+++ .hooks/helpful_hints.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import os
+import re
+
+
+def main():
+
+    print("If you get any linting errors, please look at the gdlintrc file for the rules to follow.")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
