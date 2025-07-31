2d2c8172ff6df444be071546aa8cb32558edb39f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-31 16:37:23 +0200

    Add variable wind impact

--- src/modules/minigame/wind_platformer/player.gd
+++ src/modules/minigame/wind_platformer/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var acceleration: float = 100.0
 @export var jump_speed: float = -100.0
 @export var air_control: float = 0.25
+@export var wind_impact: float = 1.0
 @export var gravity: float = 100.0
 @export var damping: float = 0.5
 
@@ -20,13 +21,13 @@ func _physics_process(delta: float) -> void:
 	var is_on_ground: bool = false
 
 	is_on_ground = is_on_floor()
-	hat.visible = not is_on_ground
+	#hat.visible = not is_on_ground
 
 	var hor_input = Input.get_axis("ui_left", "ui_right")
 
 	if not is_on_ground:
 		velocity.y += gravity * delta
-		velocity += game.get_force_at(position)
+		velocity += game.get_force_at(position) * wind_impact
 
 		velocity.x += hor_input * air_control * delta
 	else:
