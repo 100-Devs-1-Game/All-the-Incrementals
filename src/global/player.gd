cf26caffe6bf4d7fd6b73901f6c14ab60437862b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-01 23:46:18 +0200

    Add global Player

---
+++ src/global/player.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var data: PlayerState
+
+
+func _init():
+	EventBus.game_loaded.connect(func(world_state: WorldState): data = world_state.player_state)
+
+
+func add_stack_to_inventory(stack: EssenceStack):
+	if not data:
+		push_error("Player data not set")
+		return
+	data.inventory.add_stack(stack)
+
+
+func can_afford(_cost: EssenceStack) -> bool:
+	if not data:
+		push_error("Player data not set")
+		# assuming this is a test session
+		return true
+	return false
+	# TODO not implemented yet
+	#
+	#return data.inventory.has_all(cost)
