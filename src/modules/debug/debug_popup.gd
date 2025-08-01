1931aaf1770bd4097a899343a7157f82bf5926f1
microlancer <microlancer@users.noreply.github.com>
2025-08-01 14:34:55 +0900

    Replace Placeholder class with DebugPopup

--- src/modules/debug/debug_popup.gd
+++ src/modules/debug/debug_popup.gd
@@ -12,17 +12,40 @@ extends Control
 func _ready() -> void:
 	# By default, the debug popup is not visible. Press 'X' to bring it up.
 	visible = visible_on_start
+	_setup_debug_buttons()
 
+
+func _setup_debug_buttons() -> void:
 	for debug_button in debug_buttons:
 		var new_button = Button.new()
-		new_button.connect(
-			"pressed", Callable(self, "_button_pressed").bind(debug_button.function_name)
-		)
+		_add_button(debug_button, new_button)
 		$ShortcutButtons.add_child(new_button)
-		if debug_button.hotkey != "":
-			new_button.text = "[" + debug_button.hotkey + "] " + debug_button.function_name + "()"
-		else:
-			new_button.text = debug_button.function_name + "()"
+
+
+func _add_button(debug_button: DebugButton, new_button: Button) -> void:
+	if debug_button.func_or_path.begins_with("res://"):
+		_add_change_scene_button(debug_button, new_button)
+	else:
+		_add_function_call_button(debug_button, new_button)
+
+
+func _add_change_scene_button(debug_button: DebugButton, new_button: Button) -> void:
+	var scene_path = debug_button.func_or_path
+	var scene_filename = scene_path.get_file()
+	new_button.connect("pressed", Callable(self, "_change_scene").bind(scene_path))
+	if debug_button.hotkey != "":
+		new_button.text = "[" + debug_button.hotkey + "] go to " + scene_filename
+	else:
+		new_button.text = "go to " + scene_filename
+
+
+func _add_function_call_button(debug_button: DebugButton, new_button: Button) -> void:
+	var function_name = debug_button.func_or_path
+	new_button.connect("pressed", Callable(self, "_call_function").bind(function_name))
+	if debug_button.hotkey != "":
+		new_button.text = "[" + debug_button.hotkey + "] " + function_name + "()"
+	else:
+		new_button.text = function_name + "()"
 
 
 func _input(event: InputEvent) -> void:
@@ -34,13 +57,30 @@ func _input(event: InputEvent) -> void:
 			print(event)
 		for debug_button in debug_buttons:
 			if debug_button.hotkey.to_lower() == event.as_text_keycode().to_lower():
-				_button_pressed(debug_button.function_name)
+				if debug_button.func_or_path.begins_with("res://"):
+					_change_scene(debug_button.func_or_path)
+				else:
+					_call_function(debug_button.func_or_path)
 
 
-func _button_pressed(function_name: String) -> void:
+func _call_function(function_name: String) -> void:
+	if function_name == "":
+		push_error("No function name set for debug button.")
+		return
+
 	var parent = get_parent()
-	if parent != null:
-		print("Calling " + function_name + "() of the parent node.")
-		parent.call(function_name)
-	else:
-		print("Missing parent node - cannot call function " + function_name + "()")
+	if parent == null:
+		push_error("Missing parent node - cannot call function " + function_name + "()")
+		return
+
+	print("Calling " + function_name + "() of the parent node.")
+	parent.call(function_name)
+
+
+func _change_scene(scene_path: String) -> void:
+	if scene_path == "":
+		push_error("No scene_path defined for placeholder button target")
+		return
+
+	print("Changing scene to: " + scene_path)
+	get_tree().change_scene_to_packed(load(scene_path))
