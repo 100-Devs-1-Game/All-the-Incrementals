ec260f0c8d5fc0eff0834899dfbb1d00f3b4064c
Plant <99131806+PlantDev@users.noreply.github.com>
2025-08-01 03:14:00 -0300

    Settings funcionality & Cleanut
    This adds a functional settings panel, we can likely reuse it for the in game pause menu later if we have one. Cleaned it up a good amount.

---
+++ src/modules/settings/game_settings.gd
@@ -0,0 +1,162 @@
+extends Node
+
+#Setting data for saving,loading and resetting settings to default. Stores data.
+
+# Video Settings
+var fullscreen: bool = false
+var quality: String = "Best" #Low, Medium, High, Best
+
+# Audio Settings
+var master_volume: float = 50.0
+var music_volume: float = 50.0
+var sfx_volume: float = 50.0
+
+# Buttons for rebinding
+var keybinds := {
+	"primary_action": [KEY_SPACE, KEY_Z],
+	"secondary_action": [KEY_E, KEY_X],
+	"right": [KEY_D, KEY_RIGHT],
+	"down": [KEY_S, KEY_DOWN],
+	"left": [KEY_A, KEY_LEFT],
+	"up": [KEY_W, KEY_UP],
+	"exit_menu": [KEY_ESCAPE, KEY_C],
+}
+
+var action_names := {
+	1: {"name": "primary_action", "index": 0}, #Primary, 1st key
+	2: {"name": "primary_action", "index": 1}, #Primary, 2nd key
+	3: {"name": "secondary_action", "index": 0}, #Secondary, 1st key
+	4: {"name": "secondary_action", "index": 1}, #Secondary, 2nd key and so on
+	5: {"name": "right", "index": 0},
+	6: {"name": "right", "index": 1},
+	7: {"name": "down", "index": 0},
+	8: {"name": "down", "index": 1},
+	9: {"name": "left", "index": 0},
+	10: {"name": "left", "index": 1},
+	11: {"name": "up", "index": 0},
+	12: {"name": "up", "index": 1},
+	13: {"name": "exit_menu", "index": 0},
+	14: {"name": "exit_menu", "index": 1}
+}
+
+var save_path = "user://settings.cfg"
+
+func _ready() -> void:
+	load_settings()
+
+func set_audio(bus: int, volume: float):
+	match bus:
+		0:
+			master_volume = volume * 100.0
+		1:
+			music_volume = volume * 100.0
+		2:
+			sfx_volume = volume * 100.0
+	AudioServer.set_bus_volume_db(bus, volume)
+	print("Audio adjusted: ", bus, volume)
+
+func set_fullscreen(state: bool) -> void:
+	fullscreen = state
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+## Saves current setting configurations to user://settings.cfg
+func save_settings():
+	var config = ConfigFile.new()
+	config.set_value("video", "fullscreen", fullscreen)
+	config.set_value("video", "quality", quality)
+	
+	config.set_value("audio", "master_volume", master_volume)
+	config.set_value("audio", "music_volume", music_volume)
+	config.set_value("audio", "sfx_volume", sfx_volume)
+	
+	var str_keybinds := {}
+	for key in keybinds:
+		str_keybinds[key] = []
+		for keycode in keybinds[key]:
+			str_keybinds[key].append(OS.get_keycode_string(keycode))
+	
+	config.set_value("controls", "keybinds", str_keybinds)
+	
+	var err := config.save(save_path)
+	if err == OK:
+		print("Settings saved to: ", save_path)
+	else:
+		print("Error, settings failed to save.")
+
+## Loads previous setting configurations from usser://settings.cfg
+func load_settings():
+	var config = ConfigFile.new()
+	var err := config.load(save_path)
+	
+	if err != OK:
+		print("No previous save found.")
+		return
+	
+	fullscreen = config.get_value("video", "fullscreen", fullscreen)
+	quality = config.get_value("video", "quality", quality)
+	
+	master_volume = config.get_value("audio", "master_volume", master_volume)
+	music_volume = config.get_value("audio", "music_volume", music_volume)
+	sfx_volume = config.get_value("audio", "sfx_volume", sfx_volume)
+	
+	var saved_binds = config.get_value("controls", "keybinds", keybinds)
+	keybinds.clear()
+	
+	for key in saved_binds:
+		keybinds[key] = []
+		for key_name in saved_binds[key]:
+			keybinds[key].append(OS.find_keycode_from_string(key_name))
+
+## Sets settings back to the defaults then saves them to file automatically
+func restore_defaults():
+	fullscreen = false
+	quality = "Best"
+	master_volume = 50.0
+	music_volume = 50.0
+	sfx_volume = 50.0
+	keybinds = {
+		"primary_action": [KEY_SPACE, KEY_Z],
+		"secondary_action": [KEY_E, KEY_X],
+		"right": [KEY_D, KEY_RIGHT],
+		"down": [KEY_S, KEY_DOWN],
+		"left": [KEY_A, KEY_LEFT],
+		"up": [KEY_W, KEY_UP],
+		"exit_menu": [KEY_ESCAPE, KEY_C]
+		}
+	#apply_new_inputmap()
+	save_settings()
+
+## Requests a keybind change from an old key to a new one
+func rebind_request(action: String, old_key: InputEventKey, new_key: InputEventKey, action_id) -> bool:
+	var success := true
+	var old_keycode = old_key.keycode
+	var new_keycode = new_key.keycode
+	print("Request recieved: ", action , " to remove ", old_key.as_text(), " for ", new_key.as_text())
+	for bind_list in keybinds.values():
+		if new_keycode in bind_list:
+			print("This key is already in use by another action you peasant >:(")
+			return false
+			break
+	
+	InputMap.action_erase_event(action, old_key)
+	InputMap.action_add_event(action, new_key)
+	var index = action_names[action_id]["index"]
+	keybinds[action][index] = new_key.keycode
+	return success
+
+## Applies the default keybinds
+func apply_new_inputmap():
+	for action in keybinds:
+		for event in InputMap.action_get_events(action):
+			InputMap.action_erase_event(action, event)
+		for keycode in keybinds[action]:
+			var ev := InputEventKey.new()
+			ev.keycode = keycode
+			InputMap.action_add_event(action, ev)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		save_settings()
--- src/modules/settings/rebinding/rebinder.gd
+++ src/modules/settings/rebinding/rebinder.gd
@@ -1,63 +1,30 @@
 extends Control
 
+@onready var InputLabel = $Panel/Panel/NewKeyLabel
 var action_id := 0
-var action_text := "[Action]"
-var action_names := {
-	1: {"name": "primary_action", "index": 0},
-	2: {"name": "primary_action", "index": 1},
-	3: {"name": "secondary_action", "index": 0},
-	4: {"name": "secondary_action", "index": 1},
-	5: {"name": "right", "index": 0},
-	6: {"name": "right", "index": 1},
-	7: {"name": "down", "index": 0},
-	8: {"name": "down", "index": 1},
-	9: {"name": "left", "index": 0},
-	10: {"name": "left", "index": 1},
-	11: {"name": "up", "index": 0},
-	12: {"name": "up", "index": 1},
-	13: {"name": "exit_menu", "index": 0},
-	14: {"name": "exit_menu", "index": 1}
-}
+var caller : Button
 var listening := true
-var old_events
-var old_action
-var pos
-var caller
-var settings
-
-func _ready() -> void:
-	_check_action_id(action_id)
-	print("Called by ", caller)
-	settings = get_parent()
-
-func _check_action_id(id: int):
-	#print(action_names.get(action_id))
-	action_text = action_names.get(action_id)["name"]
-	$Panel/KeyLabel.text = "Press any key to rebind " + str(action_names.get(action_id)["name"])
-	old_events = InputMap.action_get_events(action_text)
-	pos = action_names.get(action_id)["index"]
-	print("THis IS THE INDEX: ", pos)
-	old_action = old_events.get(action_names.get(action_id)["index"])
-	print("this: ", old_events.get(action_names.get(action_id)["index"]))
-	InputMap.action_erase_event(action_text, old_events.get(action_names.get(action_id)["index"]))
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("exit_menu"):
-		print("Rebinding cancelled")
+		print("Exiting rebinder")
 		queue_free()
 		return
 	if listening and event is InputEventKey and event.is_pressed():
-		print("Rebinding key to: ", event.as_text())
-		$Panel/Panel/NewKeyLabel.text = event.as_text()
+		var action_text = GameSettings.action_names.get(action_id)["name"]
+		var old_event = InputMap.action_get_events(action_text)
+		var old_key = old_event.get(GameSettings.action_names.get(action_id)["index"])
+		InputLabel.text = event.as_text()
 		listening = false
-		rebind_key(action_text, old_action, event)
-
-func rebind_key(action: String, old_event: InputEventKey, new_event: InputEventKey) -> void:
-	InputMap.action_erase_event(action, old_event)
-	InputMap.action_add_event(action, new_event)
-	print("Rebound ", action, " to ", new_event.as_text())
-	var index = action_names[action_id]["index"]
-	SettingsRetainer.keybinds[action][index] = new_event.keycode
-	print("Updated SettingsRetainer.keybinds: ", SettingsRetainer.keybinds)
-	caller.text = new_event.as_text()
-	queue_free()
+		if GameSettings.rebind_request(action_text, old_key, event, action_id):
+			print("That'll do donkeh, that'll do.")
+			caller.text = event.as_text()
+			await get_tree().create_timer(0.3).timeout
+			queue_free()
+		else:
+			if $Panel/Error:
+				$Panel/Error.show()
+				await get_tree().create_timer(1.0).timeout
+				$Panel/Error.hide()
+				listening = true
+		
