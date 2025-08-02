e6858d62a2d864d0422e64dbe8a86a45388daf40
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-02 17:15:32 +0100

    camera helpers

--- src/modules/minigame/water_target_fishing/game/camera2d.gd
+++ src/modules/minigame/water_target_fishing/game/camera2d.gd
@@ -3,21 +3,51 @@ extends Camera2D
 
 @export var player: WTFPlayer
 
+var _updated: bool = false
+
 var _visible_rect: Rect2
-var _updated_rect: bool = false
 
 
-func get_visible_rect() -> Rect2:
-	if _updated_rect:
-		return _visible_rect
+func _update() -> void:
+	if _updated:
+		return
 
 	var viewport_size = get_viewport_rect().size
 	_visible_rect.position = get_screen_center_position() - viewport_size * 0.5 * zoom
 	_visible_rect.size = viewport_size * zoom
+
+	_bounds_rect.position = _visible_rect.position
+
+	_updated = true
+	return
+
+
+func get_visible_rect() -> Rect2:
+	_update()
 	return _visible_rect
 
 
+func get_left() -> float:
+	_update()
+	return _visible_rect.position.x
+
+
+func get_right() -> float:
+	_update()
+	return _visible_rect.end.x
+
+
+func get_top() -> float:
+	_update()
+	return _visible_rect.position.y
+
+
+func get_bottom() -> float:
+	_update()
+	return _visible_rect.end.y
+
+
 func _process(_delta: float) -> void:
-	_updated_rect = false
+	_updated = false
 	#todo: idk if it should follow the player tbh?
 	#global_position = player.global_position
