1d2a738c0bd9d4d10c32384564f8c9be0748e60e
Nicolai Moore <niconorsk@gmail.com>
2025-08-02 14:03:24 +1000

    Added initial attempt at the minigame resource files with some upgrades

---
+++ src/modules/minigame/earth_potato_herding/earth_potato_herding.gd
@@ -0,0 +1,11 @@
+class_name EarthPotatoHerdingMinigame
+extends BaseMinigame
+
+# Upgrade signals
+signal nutritious_potato(value: float)
+signal spirit_keeper_brightness(value: float)
+signal spirit_keeper_speed(value: float)
+
+
+func _start() -> void:
+	pass
--- src/modules/minigame/earth_potato_herding/eph_player/eph_player.gd
+++ src/modules/minigame/earth_potato_herding/eph_player/eph_player.gd
@@ -2,8 +2,19 @@ extends TopDown2DCharacterController
 
 #region ======================== PRIVATE METHODS ===============================
 
+const BASE_PLAYER_SPEED = Vector2(300.0, 300.0)
+
+
+func _ready() -> void:
+	get_tree().root.connect("spirit_keeper_speed", _on_player_speed_changed)
+
 
 func _on_evil_spirit_repel_area_body_entered(body: Node2D) -> void:
 	body.queue_free()
+
+
+func _on_player_speed_changed(modifier: float):
+	_base_speed.x = BASE_PLAYER_SPEED.x * modifier
+	_base_speed.y = BASE_PLAYER_SPEED.y * modifier
 
 #endregion
