d72ccda61fae926d47c7db9596c1de385258d220
microlancer <microlancer@users.noreply.github.com>
2025-08-03 00:56:16 +0900

    Launch more minigames with settlement and add skeleton for MinigameData to remaining

--- src/modules/overworld_locations/breezekiln/breezekiln.gd
+++ src/modules/overworld_locations/breezekiln/breezekiln.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 
+func debug_start_fire_cooking() -> void:
+	var data: MinigameData = load("res://modules/minigame/fire_cooking/data/fire_cooking_data.tres")
+	_debug_force_settlement_data()
+	SceneLoader.start_minigame(data)
+
+
 func debug_start_fire_fighters() -> void:
 	var data: MinigameData = load(
 		"res://modules/minigame/fire_fighters/data/fire_fighters_data.tres"
@@ -13,6 +19,12 @@ func debug_start_wind_platformer() -> void:
 	var data: MinigameData = load(
 		"res://modules/minigame/wind_platformer/data/wind_platformer_data.tres"
 	)
+	_debug_force_settlement_data()
+	SceneLoader.start_minigame(data)
+
+
+func debug_start_wind_rhythm() -> void:
+	var data: MinigameData = load("res://modules/minigame/wind_rhythm/data/wind_rhythm_data.tres")
 	_debug_force_settlement_data()
 	SceneLoader.start_minigame(data)
 
--- src/modules/overworld_locations/clayport/clayport.gd
+++ src/modules/overworld_locations/clayport/clayport.gd
@@ -9,6 +9,26 @@ func debug_start_earth_potato_herding() -> void:
 	SceneLoader.start_minigame(data)
 
 
+func debug_start_earth_towers() -> void:
+	var data: MinigameData = load("res://modules/minigame/earth_towers/data/earth_towers_data.tres")
+	_debug_force_settlement_data()
+	SceneLoader.start_minigame(data)
+
+
+func debug_start_water_diving() -> void:
+	var data: MinigameData = load("res://modules/minigame/water_diving/data/water_diving_data.tres")
+	_debug_force_settlement_data()
+	SceneLoader.start_minigame(data)
+
+
+func debug_start_water_target_fishing() -> void:
+	var data: MinigameData = load(
+		"res://modules/minigame/water_target_fishing/data/water_target_fishing_data.tres"
+	)
+	_debug_force_settlement_data()
+	SceneLoader.start_minigame(data)
+
+
 # For temporary debugging purposes, directly set the current settlement. Once
 # we have the overworld code working, we'll be setting the settlement naturally.
 func _debug_force_settlement_data() -> void:
