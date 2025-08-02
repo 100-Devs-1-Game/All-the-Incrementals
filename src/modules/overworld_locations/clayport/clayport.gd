dd41807236c23fa1a42aa1f09bc6a9846e65a33c
microlancer <microlancer@users.noreply.github.com>
2025-08-03 01:16:08 +0900

    Use exports for resources

--- src/modules/overworld_locations/clayport/clayport.gd
+++ src/modules/overworld_locations/clayport/clayport.gd
@@ -1,36 +1,35 @@
 extends Node2D
 
+@export var earth_potato_herding_data: MinigameData
+@export var earth_towers_data: MinigameData
+@export var water_diving_data: MinigameData
+@export var water_target_fishing_data: MinigameData
+
+
+func _ready() -> void:
+	_debug_force_settlement_data()
+
 
 func debug_start_earth_potato_herding() -> void:
-	var data: MinigameData = load(
-		"res://modules/minigame/earth_potato_herding/data/earth_potato_herding_data.tres"
-	)
-	_debug_force_settlement_data()
-	SceneLoader.start_minigame(data)
+	SceneLoader.start_minigame(earth_potato_herding_data)
 
 
 func debug_start_earth_towers() -> void:
-	var data: MinigameData = load("res://modules/minigame/earth_towers/data/earth_towers_data.tres")
-	_debug_force_settlement_data()
-	SceneLoader.start_minigame(data)
+	SceneLoader.start_minigame(earth_towers_data)
 
 
 func debug_start_water_diving() -> void:
-	var data: MinigameData = load("res://modules/minigame/water_diving/data/water_diving_data.tres")
-	_debug_force_settlement_data()
-	SceneLoader.start_minigame(data)
+	SceneLoader.start_minigame(water_diving_data)
 
 
 func debug_start_water_target_fishing() -> void:
-	var data: MinigameData = load(
-		"res://modules/minigame/water_target_fishing/data/water_target_fishing_data.tres"
-	)
-	_debug_force_settlement_data()
-	SceneLoader.start_minigame(data)
+	SceneLoader.start_minigame(water_target_fishing_data)
 
 
 # For temporary debugging purposes, directly set the current settlement. Once
 # we have the overworld code working, we'll be setting the settlement naturally.
 func _debug_force_settlement_data() -> void:
-	var data: SettlementData = load("res://modules/overworld_locations/clayport/clayport_data.tres")
-	SceneLoader._current_settlement = data
+	var clayport_data: SettlementData = load(
+		"res://modules/overworld_locations/clayport/clayport_data.tres"
+	)
+	SceneLoader._current_settlement = clayport_data
