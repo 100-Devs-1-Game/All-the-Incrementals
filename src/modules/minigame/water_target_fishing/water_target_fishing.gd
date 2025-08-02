74d4900519e8e669d3e6700c42211b5dcf2cde4f
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-03 00:14:12 +0100

    update minigame to use stats

--- src/modules/minigame/water_target_fishing/water_target_fishing.gd
+++ src/modules/minigame/water_target_fishing/water_target_fishing.gd
@@ -2,29 +2,30 @@ class_name WTFMinigame
 extends BaseMinigame
 
 const SECONDS_BEFORE_ENDING_RUN: float = 1
-const SPAWN_FISH_EVERY_X_PIXELS_TRAVELLED: float = 300
-const SPAWN_STARTING_FISH: float = 3
 
 @export var fish: PackedScene
 
-var current_velocity: Vector2 = Vector2(-200, 0)
-var base_slow: float = 100
+var stats: WTFStats
 
-var distance_travelled = 0
-var distance_travelled_left_to_spawn = SPAWN_FISH_EVERY_X_PIXELS_TRAVELLED * SPAWN_STARTING_FISH
-var score_weight_modifier = 3
-
-var _pixels_per_second: float = 0
+var _distance_travelled = 0
+var _distance_since_spawned = 0
 
 @onready var fish_db: WTFFishDB = %WTFFishDB
 
+@onready var ui_speed_value: RichTextLabel = %UISpeedValue
+@onready var ui_score_value: RichTextLabel = %UIScoreValue
+@onready var ui_oxygen_value: RichTextLabel = %UIOxygenValue
+@onready var ui_weight_value: RichTextLabel = %UIWeightValue
+@onready var ui_distance_value: RichTextLabel = %UIDistanceValue
+
 
 func get_pixels_per_second() -> int:
-	return floor(_pixels_per_second)
+	return floori(-stats.scrollspeed.x)
 
 
 func _enter_tree() -> void:
 	WTFGlobals.minigame = self
+	stats = WTFStats.new()
 
 	## yyyy does it stutter thoooo
 	# I can't find any magic number that solves it :(
@@ -36,27 +37,17 @@ func _exit_tree() -> void:
 	WTFGlobals.minigame = null
 
 
+func _start() -> void:
+	# give it an initial amount so we can get some fishies going
+	_distance_since_spawned = stats.spawn_fish_every_x_pixels * stats.spawn_x_starting_fish
+
+
 func _process(_delta: float) -> void:
-	(%UISpeedValue as RichTextLabel).text = str(get_pixels_per_second())
-	(%UIScoreValue as RichTextLabel).text = str(score)
-	(%UIOxygenValue as RichTextLabel).text = (
-		str(
-			floori(
-				(
-					(
-						max(0, WTFGlobals.player.oxygen_remaining_seconds)
-						/ WTFGlobals.player.oxygen_capacity_seconds
-					)
-					* 100.0
-				)
-			)
-		)
-		+ "%"
-	)
-	(%UIWeightValue as RichTextLabel).text = str(
-		floori(base_slow + (score * score_weight_modifier))
-	)
-	(%UIDistanceValue as RichTextLabel).text = str(floori(distance_travelled))
+	ui_speed_value.text = str(get_pixels_per_second())
+	ui_score_value.text = str(score)
+	ui_oxygen_value.text = str(WTFGlobals.minigame.stats.oxygen_percentage()) + "%"
+	ui_weight_value.text = str(floori(stats.total_weight()))
+	ui_distance_value.text = str(floori(_distance_travelled))
 
 
 func _spawn_fish() -> void:
@@ -65,32 +56,28 @@ func _spawn_fish() -> void:
 		return
 
 	var f: WTFFish = fish.instantiate()
-	var rand_offset := randf_range(-current_velocity.x, -current_velocity.x * 4)
-	f.position.x = (distance_travelled + WTFGlobals.camera.get_right() + rand_offset)  #avoid clump
+	var rand_offset := randf_range(-stats.scrollspeed.x, -stats.scrollspeed.x * 4)
+	f.position.x = (_distance_travelled + WTFGlobals.camera.get_right() + rand_offset)  #avoid clump
 	f.position.y = randf_range(min(0, WTFGlobals.camera.get_top()), WTFGlobals.camera.get_bottom())
 	f.data = fish_db.random()
 	%Entities.add_child(f)
 
 
 func _physics_process(delta: float) -> void:
-	current_velocity.x += (base_slow + (score * score_weight_modifier)) * delta
+	stats.scroll_slower(stats.total_weight() * delta)
 
 	# effectively stopped
-	if current_velocity.x >= 0:
-		current_velocity.x = 0
-
+	if !stats.scrolling():
 		#todo end the run and show summary or ui to restart or buy upgrades
 		# hack that acts as a one second timer before the run ends
-		var player_done := WTFGlobals.player.oxygen_remaining_seconds <= -SECONDS_BEFORE_ENDING_RUN
+		var player_done := stats.oxygen_remaining_seconds <= -SECONDS_BEFORE_ENDING_RUN
 		if !WTFGlobals.player.underwater() && player_done:
 			get_tree().reload_current_scene()
 
-	_pixels_per_second = -current_velocity.x
-
-	distance_travelled += _pixels_per_second * delta
-	distance_travelled_left_to_spawn += _pixels_per_second * delta
+	_distance_travelled += -1 * stats.scrollspeed.x * delta
+	_distance_since_spawned += -1 * stats.scrollspeed.x * delta
 
 	#todo replace with good spawning
-	while distance_travelled_left_to_spawn > SPAWN_FISH_EVERY_X_PIXELS_TRAVELLED:
+	while _distance_since_spawned > stats.spawn_fish_every_x_pixels:
 		_spawn_fish()
-		distance_travelled_left_to_spawn -= SPAWN_FISH_EVERY_X_PIXELS_TRAVELLED
+		_distance_since_spawned -= stats.spawn_fish_every_x_pixels
