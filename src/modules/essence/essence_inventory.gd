31cc194cec203493406b75f27bd59da96614d13a
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-02 18:46:13 +0200

    Improve usability

--- src/modules/essence/essence_inventory.gd
+++ src/modules/essence/essence_inventory.gd
@@ -1,13 +1,64 @@
+@tool
 class_name EssenceInventory
 extends Resource
 
+#
+# Introduce hard-coding of essences so this Resource is easier
+# to use in the Inspector, without having to edit nested Resources
+
+const EARTH_ESSENCE = preload("res://modules/essence/resources/earth.tres")
+const FIRE_ESSENCE = preload("res://modules/essence/resources/fire.tres")
+const WATER_ESSENCE = preload("res://modules/essence/resources/water.tres")
+const WIND_ESSENCE = preload("res://modules/essence/resources/wind.tres")
+
+@export var earth: int:
+	set(n):
+		earth = n
+		_remove_and_add_essence(EARTH_ESSENCE, n)
+
+@export var fire: int:
+	set(n):
+		fire = n
+		_remove_and_add_essence(FIRE_ESSENCE, n)
+
+@export var water: int:
+	set(n):
+		water = n
+		_remove_and_add_essence(WATER_ESSENCE, n)
+
+@export var wind: int:
+	set(n):
+		wind = n
+		_remove_and_add_essence(WIND_ESSENCE, n)
+
 @export var slots: Array[EssenceStack]
+
+
+func _remove_and_add_essence(essence: Essence, n: int):
+	# so changes don't get added
+	remove_essence(essence)
+	add_essence(essence, n)
 
 
 func add_stack(stack: EssenceStack):
 	for slot in slots:
 		if slot.essence == stack.essence:
 			slot.merge(stack)
+			return
+	slots.append(stack)
+
+
+# convenience function
+func add_essence(essence: Essence, amount: int):
+	if amount < 1:
+		return
+	add_stack(EssenceStack.new(essence, amount))
+
+
+func remove_essence(essence: Essence):
+	for slot in slots:
+		if slot.essence == essence:
+			slots.erase(slot)
 			return
 
 # TODO sub_stack, has_essence, has_stack, merge, ...
