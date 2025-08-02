2aea7833b4cbf8d9b29aff4e7ede0d856fdf848d
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-03 00:12:05 +0100

    remove pickup helper

--- src/modules/minigame/water_target_fishing/game/pickup_data.gd
+++ src/modules/minigame/water_target_fishing/game/pickup_data.gd
@@ -4,7 +4,3 @@ extends Resource
 @export var pixels_per_second: float
 @export var weight: float
 @export var score: float
-
-
-func get_velocity_change() -> Vector2:
-	return Vector2(-pixels_per_second, 0)
