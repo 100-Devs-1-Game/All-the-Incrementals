29fd7158cbe5f396b3f650928693eae139410aaf
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-03 05:05:25 +0200

    Update fire_fighters_item_spawner.gd

--- src/modules/minigame/fire_fighters/fire_fighters_item_spawner.gd
+++ src/modules/minigame/fire_fighters/fire_fighters_item_spawner.gd
@@ -3,6 +3,7 @@ extends Node
 
 @export var item_instance_scene: PackedScene
 @export var debug_items: Array[FireFightersMinigameItem]
+@export var spawn_tries: int = 10
 
 var active_items: Array[FireFightersMinigameItem]
 
@@ -18,15 +19,22 @@ func activate():
 func run():
 	for item in _get_all_items():
 		if RngUtils.chancef(item.get_spawn_probability()):
-			spawn_item(item)
+			for i in spawn_tries:
+				var spawn_tile: Vector2i = game.get_random_tile()
+				if _can_spawn_item_on(spawn_tile):
+					spawn_item(item, game.get_tile_position(spawn_tile))
+					break
 
 
-func spawn_item(item_type: FireFightersMinigameItem):
+func spawn_item(item_type: FireFightersMinigameItem, pos: Vector2):
 	var item: FireFightersMinigameItemInstance = item_instance_scene.instantiate()
-	var spawn_tile: Vector2i = game.get_random_tile()
-	item.position = game.get_tile_position(spawn_tile)
+	item.position = pos
 	game.item_node.add_child(item)
 	item.init(item_type)
+
+
+func _can_spawn_item_on(_tile: Vector2i) -> bool:
+	return true
 
 
 func _get_all_items() -> Array[FireFightersMinigameItem]:
