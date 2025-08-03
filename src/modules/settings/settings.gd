c0131542c84211ff353879eefaa6fed66bb5f3f0
Plant <99131806+PlantDev@users.noreply.github.com>
2025-08-03 05:48:14 -0300

    Revamp settings UI layout and visuals
    Major update to the settings and keybinding UI, including new shader-based backgrounds, improved panel and button styling, and a reorganized node structure for better clarity and usability. Updated script references and node paths to match the new layout, and added support for more flexible scene transitions in settings.gd.

--- src/modules/settings/settings.gd
+++ src/modules/settings/settings.gd
@@ -2,6 +2,7 @@ extends Control
 
 const REBINDER = preload("res://modules/settings/rebinding/rebinder.tscn")
 
+var action_menu := false #Enable to change Apply&Exit behaviour to queue_free instead of scene change
 var fullscreen := false
 var ordered_actions := [
 	"primary_action",
@@ -21,19 +22,19 @@ var ordered_actions := [
 ]
 
 @onready var setting_buttons := {
-	$Panel/SettingsContainer/QualityContainer/LowQualityButton: "Low",
-	$Panel/SettingsContainer/QualityContainer/MedQualityButton: "Medium",
-	$Panel/SettingsContainer/QualityContainer/HighQualityButton: "High",
-	$Panel/SettingsContainer/QualityContainer/BestQualityButton: "Best",
-	$Panel/SettingsContainer/ScreenButton: "Fullscreen",
+	$Panel/SettingsContainer/MultiContainer/HBoxContainer/Low: "Low",
+	$Panel/SettingsContainer/MultiContainer/HBoxContainer/Medium: "Medium",
+	$Panel/SettingsContainer/MultiContainer/HBoxContainer/High: "High",
+	$Panel/SettingsContainer/MultiContainer/HBoxContainer/Best: "Best",
+	$Panel/SettingsContainer/MultiContainer/FullscreenContainer/ScreenButton: "Fullscreen",
 	$BackButton: "Exit",
 	$RestoreButton: "Restore"
 }
 
 @onready var audio_sliders := {
-	$Panel/SettingsContainer/MasterContainer/MasterSlider: "Master",
-	$Panel/SettingsContainer/MusicContainer/MusicSlider: "Music",
-	$Panel/SettingsContainer/SFXContainer/SFXSlider: "SFX"
+	$Panel/SettingsContainer/MultiContainer/Masterlbl/MasterSlider: "Master",
+	$Panel/SettingsContainer/MultiContainer/Musiclbl/MusicSlider: "Music",
+	$Panel/SettingsContainer/MultiContainer/SFXlbl/SFXSlider: "SFX"
 }
 
 
@@ -64,6 +65,7 @@ func connect_signals():
 
 
 func _on_button_pressed(action: String) -> void:
+	update_ui()
 	match action:
 		"Low", "Medium", "High", "Best":
 			GameSettings.quality = action
@@ -72,12 +74,13 @@ func _on_button_pressed(action: String) -> void:
 			print(fullscreen)
 			GameSettings.set_fullscreen(fullscreen)
 		"Exit":
-			get_tree().change_scene_to_packed(load("res://modules/menu/main_menu.tscn"))
+			if !action_menu:
+				get_tree().change_scene_to_packed(load("res://modules/menu/main_menu.tscn"))
+			else:
+				queue_free()
 		"Restore":
 			GameSettings.restore_defaults()
 			_on_button_pressed("Exit")
-	update_ui()
-
 
 func _on_volume_changed(value: float, type: String) -> void:
 	var balanced = clamp(value / 100.0, 0.001, 1.0)
@@ -108,22 +111,22 @@ func _input(event: InputEvent) -> void:
 func update_ui():
 	print(GameSettings.keybinds)
 	if GameSettings.fullscreen:
-		$Panel/SettingsContainer/ScreenButton.text = "Fullscreen: " + "ON"
+		$Panel/SettingsContainer/MultiContainer/FullscreenContainer/ScreenButton.text = "Fullscreen: " + "ON"
 	else:
-		$Panel/SettingsContainer/ScreenButton.text = "Fullscreen: " + "OFF"
+		$Panel/SettingsContainer/MultiContainer/FullscreenContainer/ScreenButton.text = "Fullscreen: " + "OFF"
 
-	$Panel/SettingsContainer/QualityContainer/QualityLabel.text = (
+	$Panel/SettingsContainer/MultiContainer/QualityLabel.text = (
 		"3D Quality: " + str(GameSettings.quality)
 	)
-	$Panel/SettingsContainer/MasterContainer/MasterSlider.value = GameSettings.master_volume
-	$Panel/SettingsContainer/MusicContainer/MusicSlider.value = GameSettings.music_volume
-	$Panel/SettingsContainer/SFXContainer/SFXSlider.value = GameSettings.sfx_volume
-	$Panel/SettingsContainer/MasterContainer/MasterSlider/ValueLabel.text = str(
-		roundi(GameSettings.master_volume)
+	$Panel/SettingsContainer/MultiContainer/Masterlbl/MasterSlider.value = GameSettings.master_volume
+	$Panel/SettingsContainer/MultiContainer/Musiclbl/MusicSlider.value = GameSettings.music_volume
+	$Panel/SettingsContainer/MultiContainer/SFXlbl/SFXSlider.value = GameSettings.sfx_volume
+	$Panel/SettingsContainer/MultiContainer/Masterlbl/MasterSlider/ValueLabel.text = str(
+		roundi(GameSettings.master_volume),  "%"
 	)
-	$Panel/SettingsContainer/MusicContainer/MusicSlider/ValueLabel.text = str(
-		roundi(GameSettings.music_volume)
+	$Panel/SettingsContainer/MultiContainer/Musiclbl/MusicSlider/ValueLabel.text = str(
+		roundi(GameSettings.music_volume), "%"
 	)
-	$Panel/SettingsContainer/SFXContainer/SFXSlider/ValueLabel.text = str(
-		roundi(GameSettings.sfx_volume)
+	$Panel/SettingsContainer/MultiContainer/SFXlbl/SFXSlider/ValueLabel.text = str(
+		roundi(GameSettings.sfx_volume), "%"
 	)
