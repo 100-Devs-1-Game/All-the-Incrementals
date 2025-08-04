7aaf6576e1b88094f3d9842b3250f6019b5557d0
Plant <99131806+PlantDev@users.noreply.github.com>
2025-08-04 01:56:17 -0300

    Update tower_base.gd

--- src/modules/minigame/earth_tower_tumble/tower_base.gd
+++ src/modules/minigame/earth_tower_tumble/tower_base.gd
@@ -6,6 +6,7 @@ extends Node2D
 var base_width = 220.0
 var base_upgrade_increment = 60.0
 
+
 func _process(_delta):
 	var rect_shape = col_shape.shape as RectangleShape2D
 	if rect_shape:
@@ -16,6 +17,7 @@ func _process(_delta):
 			Vector2(extents.x, extents.y),
 			Vector2(-extents.x, extents.y)
 		]
+
 
 func _upgrade():
 	pass
