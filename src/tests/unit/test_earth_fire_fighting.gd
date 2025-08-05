4d1c0b4cfc712cf8401a3341963a3f0c2b57c493
microlancer <microlancer@users.noreply.github.com>
2025-08-05 20:48:39 +0900

    Add test for fire fighting

---
+++ src/tests/unit/test_earth_fire_fighting.gd
@@ -0,0 +1,28 @@
+extends "res://addons/gut/test.gd"
+
+
+func test_earth_fire_fighting_run() -> void:
+	var earth_fire_fighting_data: MinigameData = load(
+		"res://modules/minigame/fire_fighters/data/fire_fighters_data.tres"
+	)
+
+	var earth_fire_fighting: FireFightersMinigame = (
+		load("res://modules/minigame/fire_fighters/fire_fighters.tscn").instantiate()
+	)
+
+	SceneLoader._current_minigame = earth_fire_fighting_data
+	SceneLoader.enable_immediate_play()
+	earth_fire_fighting.data = earth_fire_fighting_data
+
+	print("Adding child")
+	add_child_autofree(earth_fire_fighting)
+
+	print("Simulating")
+	gut.simulate(earth_fire_fighting, 10, .1)
+	await wait_seconds(3)
+
+	print("Assert")
+	assert_gt(earth_fire_fighting.fires.size(), 0, "fires > 0")
+	assert_gt(earth_fire_fighting.max_water_per_fire, 0, "max_water_per_fire > 0")
+
+	earth_fire_fighting.queue_free()
