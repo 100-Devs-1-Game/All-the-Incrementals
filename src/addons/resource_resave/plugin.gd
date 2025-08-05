56c9d3a85de1d40136245b3609680b44405ea2a0
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-05 15:19:47 +0100

    Add autoresave option & manual button

--- src/addons/resource_resave/plugin.gd
+++ src/addons/resource_resave/plugin.gd
@@ -1,23 +1,55 @@
 @tool
 extends EditorPlugin
 
+const BUTTON := "Resave All Resources"
+const SETTING_AUTORESAVE := "resource_resaver/auto_resave"
+const SETTING_AUTORESAVE_DEFAULT_VAL := false
+
+var button: Button
+
+func _enter_tree() -> void:
+	if not ProjectSettings.has_setting(SETTING_AUTORESAVE):
+		ProjectSettings.set_setting(SETTING_AUTORESAVE, SETTING_AUTORESAVE_DEFAULT_VAL)
+		ProjectSettings.set_initial_value(SETTING_AUTORESAVE, SETTING_AUTORESAVE_DEFAULT_VAL)
+		ProjectSettings.set_as_basic(SETTING_AUTORESAVE, true)
+		ProjectSettings.save()
+
+	button = Button.new()
+	button.text = BUTTON
+	button.tooltip_text = "Manually resave all .tres and .res resources"
+	button.pressed.connect(_on_resave_pressed)
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, button)
+
+
+func _exit_tree() -> void:
+	EditorInterface.get_resource_filesystem().filesystem_changed.disconnect(_on_filesystem_changed)
+	button.queue_free()
+
+
 func _ready() -> void:
-	EditorInterface.get_resource_filesystem().filesystem_changed.connect(_on_filesystem_changed())
+	EditorInterface.get_resource_filesystem().filesystem_changed.connect(_on_filesystem_changed)
+
+
+func _on_resave_pressed() -> void:
+	resave("Resource Resaver - Resaving all Resources")
 
 
 func _on_filesystem_changed():
-	print("Resource Resaver - Filesystem Change, Resaving all Resources")
-	_resave_resources("res://")
+	autoresave("Resource Resaver - Filesystem Change, Resaving all Resources")
 
 
 func _save_external_data():
-	print("Resource Resaver - Detected Save, Resaving all Resources")
-	# bit wordy innit
-#	print(
-#		"Note: Internal godot errors may appear in the output window because of the Resave\n"
-#		+ "This is intentional - it indicates a Resource that did need to be resaved\n"
-#		+ "Please double-check `git diff` for these resaved differences"
-#	)
+	autoresave("Resource Resaver - Detected Save, Resaving all Resources")
+
+
+func autoresave(msg: String) -> void:
+	var autoresave_enabled := ProjectSettings.get_setting(SETTING_AUTORESAVE, SETTING_AUTORESAVE_DEFAULT_VAL)
+	if autoresave_enabled:
+		resave(msg)
+
+
+func resave(msg: String) -> void:
+	print(msg)
 	_resave_resources("res://")
 
 
