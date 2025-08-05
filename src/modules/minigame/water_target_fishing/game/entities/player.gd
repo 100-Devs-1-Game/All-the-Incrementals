7b37ff31ad7d9f0e8224707d399c73929abc3cfa
microlancer <microlancer@users.noreply.github.com>
2025-08-05 11:29:20 +0900

    Fix inaccessible score property in water target fishing game

--- src/modules/minigame/water_target_fishing/game/entities/player.gd
+++ src/modules/minigame/water_target_fishing/game/entities/player.gd
@@ -49,7 +49,7 @@ func _on_area_entered(other_area: Area2D) -> void:
 
 	var maybe_cannon = other_area.get_parent() as WTFJetCannon
 	if is_instance_valid(maybe_cannon):
-		WTFGlobals.minigame.score += maybe_cannon.pickup.score
+		WTFGlobals.minigame.add_score(maybe_cannon.pickup.score)
 		TextFloatSystem.floating_text(
 			maybe_cannon.global_position, "+%d" % maybe_cannon.pickup.score, WTFGlobals.minigame
 		)
