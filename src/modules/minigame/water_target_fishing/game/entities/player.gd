cf60f6b3c9c4f889d71f8243e6f4b2e256ea1250
microlancer <microlancer@users.noreply.github.com>
2025-08-05 11:34:21 +0900

    Also fishes

--- src/modules/minigame/water_target_fishing/game/entities/player.gd
+++ src/modules/minigame/water_target_fishing/game/entities/player.gd
@@ -37,7 +37,7 @@ func _on_area_entered(other_area: Area2D) -> void:
 
 	var maybe_fish := other_area.get_parent() as WTFFish
 	if is_instance_valid(maybe_fish):
-		WTFGlobals.minigame.score += maybe_fish.data.pickup.score
+		WTFGlobals.minigame.add_score(maybe_fish.data.pickup.score)
 		TextFloatSystem.floating_text(
 			maybe_fish.global_position, "+%d" % maybe_fish.data.pickup.score, WTFGlobals.minigame
 		)
