ee9fbc38e8749911fa2159f2dd8e5d399eb35a58
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-06 19:39:37 +0100

    add asserts to clayport tests

--- src/tests/unit/test_clayport.gd
+++ src/tests/unit/test_clayport.gd
@@ -9,10 +9,16 @@ func test_clayport() -> void:
 	print("Adding child")
 	add_child_autofree(overworld)
 
+	var clayport = overworld.settlement_scene_holder_node.get_child(0)
+
 	print("Simulating")
 	gut.simulate(overworld, 10, .1)
 	await wait_seconds(1)
 
 	print("Assert")
+	assert_ne(clayport.earth_potato_herding_data, null, "no earth potato herding minigame data set")
+	assert_ne(clayport.earth_towers_data, null, "no earth towers minigame data set")
+	assert_ne(clayport.water_rowing_rapids_data, null, "no water rowing rapids minigame data set")
+	assert_ne(clayport.water_target_fishing_data, null, "no water target fishing minigame data set")
 
 	overworld.queue_free()
