06f14e15df168d9b2a1a195e62898113bf34181a
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-06 19:39:55 +0100

    add asserts to shrine tests

--- src/tests/unit/test_shrine.gd
+++ src/tests/unit/test_shrine.gd
@@ -9,10 +9,13 @@ func test_shrine() -> void:
 	print("Adding child")
 	add_child_autofree(overworld)
 
+	var shrine = overworld.settlement_scene_holder_node.get_child(0)
+
 	print("Simulating")
 	gut.simulate(overworld, 10, .1)
 	await wait_seconds(1)
 
 	print("Assert")
+	assert_eq(shrine.name, "Shrine", "todo: replace with real assert")
 
 	overworld.queue_free()
