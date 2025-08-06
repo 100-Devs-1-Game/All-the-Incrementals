0d62b8170f108f4fada43734e83be9041c863bc8
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-06 19:39:27 +0100

    add asserts to breezekiln tests

--- src/tests/unit/test_breezekiln.gd
+++ src/tests/unit/test_breezekiln.gd
@@ -9,10 +9,16 @@ func test_breezekiln() -> void:
 	print("Adding child")
 	add_child_autofree(overworld)
 
+	var breezekiln = overworld.settlement_scene_holder_node.get_child(0)
+
 	print("Simulating")
 	gut.simulate(overworld, 10, .1)
 	await wait_seconds(1)
 
 	print("Assert")
+	assert_ne(breezekiln.fire_cooking_data, null, "no fire cooking minigame data set")
+	assert_ne(breezekiln.fire_fighters_data, null, "no fire fighter minigame data set")
+	assert_ne(breezekiln.wind_platformer_data, null, "no wind platformer minigame data set")
+	assert_ne(breezekiln.wind_rhythm_data, null, "no wind rhythm minigame data set")
 
 	overworld.queue_free()
