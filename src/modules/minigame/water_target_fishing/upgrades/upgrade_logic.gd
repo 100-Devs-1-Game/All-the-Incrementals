0a7a3f76f5d747ce48e915f9d76df97c878e00b7
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-08 15:52:23 +0100

    WTF: UpgradeLogic

--- src/modules/minigame/water_target_fishing/upgrades/upgrade_logic.gd
+++ src/modules/minigame/water_target_fishing/upgrades/upgrade_logic.gd
@@ -1,20 +1,29 @@
 class_name WTFMinigameUpgradeLogic
 extends BaseMinigameUpgradeLogic
 
-enum UpgradeType { INVALID, TODO }
+enum UpgradeType {
+	INVALID,
+	TODO,
+	INCREASE_STARTING_FISH,
+}
 
 @export var type: UpgradeType = UpgradeType.INVALID
 
 
 func _apply_effect(_game: BaseMinigame, _upgrade: MinigameUpgrade):
-	if _game != WTFGlobals.minigame:
+	if !is_instance_valid(_game) || _game != WTFGlobals.minigame:
 		push_error("WTF - minigame is invalid")
 		assert(false)
 		return
 
+	var stats := WTFGlobals.minigame.stats
+
 	match type:
 		UpgradeType.TODO:
-			print("todo")
+			print("WTF - todo")
+		UpgradeType.INCREASE_STARTING_FISH:
+			print("WTF - increasing starting fish by %d", _upgrade.get_current_effect_modifier())
+			stats.spawn_x_starting_fish += _upgrade.get_current_effect_modifier()
 		_:
 			push_error("WTF - upgrade %s is unknown (%s)" % [_upgrade.name, _upgrade.resource_path])
 			assert(false)
