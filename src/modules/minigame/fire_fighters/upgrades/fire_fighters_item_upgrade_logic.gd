c4d8ffda9f4aff085ba45c3b95a8d67ea6427bdc
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-09 17:49:17 +0200

    Fix narrowing conversion

--- src/modules/minigame/fire_fighters/upgrades/fire_fighters_item_upgrade_logic.gd
+++ src/modules/minigame/fire_fighters/upgrades/fire_fighters_item_upgrade_logic.gd
@@ -7,4 +7,4 @@ extends BaseMinigameUpgradeLogic
 func _apply_effect(game: BaseMinigame, upgrade: MinigameUpgrade):
 	var my_game: FireFightersMinigame = game
 	my_game.item_spawner.add_item(item)
-	item.spawn_amount = upgrade.get_current_effect_modifier()
+	item.spawn_amount = int(upgrade.get_current_effect_modifier())
