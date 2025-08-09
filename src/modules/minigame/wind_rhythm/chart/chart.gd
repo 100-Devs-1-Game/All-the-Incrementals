4fecb2336048b817aa37823464c88d8a18448650
SaronTetra <starwars.maciek@gmail.com>
2025-08-10 01:33:24 +0200

    Handle note played/missed

--- src/modules/minigame/wind_rhythm/chart/chart.gd
+++ src/modules/minigame/wind_rhythm/chart/chart.gd
@@ -2,18 +2,53 @@
 class_name Chart extends Resource
 
 const JUDGMENTS = preload("res://modules/minigame/wind_rhythm/chart/judgments.gd")
-const NOTE_TYPES = preload("res://modules/minigame/wind_rhythm/chart/note_types.gd").NoteType
+const NOTE_TYPE = preload("res://modules/minigame/wind_rhythm/chart/note_types.gd").NoteType
 
 @export var name: String
 @export var audio: AudioStream
 @export var bpm: int
 @export_tool_button("Sort Bars", "Sort") var sort = sort_notes
 @export_tool_button("Calculate note times", "Time") var calculate = calculate_note_times
+@export_tool_button("Rebuild notes array", "Reload") var rebuild = _rebuild_notes_array
 @export var notes: Array[NoteData]
+@export var notes_in_bar: int = 4
+@export var lanes: Dictionary[NOTE_TYPE, Array] = {
+	NOTE_TYPE.UP: [],
+	NOTE_TYPE.LEFT: [],
+	NOTE_TYPE.RIGHT: [],
+	NOTE_TYPE.DOWN: [],
+	NOTE_TYPE.SPECIAL1: [],
+	NOTE_TYPE.SPECIAL2: [],
+}
 
 
 func _ready():
 	bpm = audio.bpm
+
+
+func _rebuild_notes_array():
+	calculate_note_times()
+	for lane in lanes.keys():
+		lanes[lane].clear()
+
+	for note: NoteData in notes:
+		if note.type & NOTE_TYPE.UP:
+			lanes[NOTE_TYPE.UP].append(note.copy({"type": NOTE_TYPE.UP}))
+		if note.type & NOTE_TYPE.LEFT:
+			lanes[NOTE_TYPE.LEFT].append(note.copy({"type": NOTE_TYPE.LEFT}))
+		if note.type & NOTE_TYPE.RIGHT:
+			lanes[NOTE_TYPE.RIGHT].append(note.copy({"type": NOTE_TYPE.RIGHT}))
+		if note.type & NOTE_TYPE.DOWN:
+			lanes[NOTE_TYPE.DOWN].append(note.copy({"type": NOTE_TYPE.DOWN}))
+		if note.type & NOTE_TYPE.SPECIAL1:
+			lanes[NOTE_TYPE.SPECIAL1].append(note.copy({"type": NOTE_TYPE.SPECIAL1}))
+		if note.type & NOTE_TYPE.SPECIAL2:
+			lanes[NOTE_TYPE.SPECIAL2].append(note.copy({"type": NOTE_TYPE.SPECIAL2}))
+
+	for lane in lanes.keys():
+		lanes[lane].sort_custom(
+			func(a: NoteData, b: NoteData): return a.cached_absolute_beat < b.cached_absolute_beat
+		)
 
 
 func sort_notes():
@@ -23,47 +58,4 @@ func sort_notes():
 func calculate_note_times():
 	if notes != null:
 		for note: NoteData in notes:
-			note.cached_absolute_beat = (note.bar + note.beat) * (60.0 / audio.bpm)
-
-
-func get_note_data_from_bar(bar: int) -> NoteData:
-	var index = notes.find_custom(func(note: NoteData): return note.bar == bar)
-	if index != -1:
-		return notes[index]
-	return null
-
-
-func get_nearest_note(time: float) -> NoteData:
-	# song time +/- lowest judgment
-	var limit = JUDGMENTS.new().okay
-	var index = notes.find_custom(
-		func(note: NoteData):
-			return (
-				note.cached_absolute_beat < (time + limit)
-				and note.cached_absolute_beat > (time - limit)
-			)
-	)
-	if index != -1:
-		print(notes[index])
-		return notes[index]
-	return null
-
-
-# Not a fan of this function, maybe we could precalculate and split notes by types?
-# Same question for replacing array with dictionary,
-# then we could access bar-1, bar, bar+1 (for overlap) directly
-# instead of iterating through array each time
-# Also we need some way to get notes past their miss window
-func get_nearest_note_by_type(time: float, type: NOTE_TYPES) -> NoteData:
-	var limit = JUDGMENTS.new().okay
-	var filtered_notes = notes.filter(func(note: NoteData): return note.type & type)
-	var index = filtered_notes.find_custom(
-		func(note: NoteData):
-			return (
-				note.cached_absolute_beat < (time + limit)
-				and note.cached_absolute_beat > (time - limit)
-			)
-	)
-	if index != -1:
-		return filtered_notes[index]
-	return null
+			note.cached_absolute_beat = (note.bar + note.beat / notes_in_bar) * (60.0 / audio.bpm)
--- src/modules/minigame/wind_rhythm/rhythm_game.gd
+++ src/modules/minigame/wind_rhythm/rhythm_game.gd
@@ -1,14 +1,24 @@
-extends Node2D
+class_name RhythmGame extends Node2D
+
+signal note_played(note: NoteData, judgment: JUDGMENT)
+signal note_missed(note: NoteData)
 
 const JUDGMENT_PATH = "res://modules/minigame/wind_rhythm/chart/judgments.gd"
 const NOTE_TYPE = preload("res://modules/minigame/wind_rhythm/chart/note_types.gd").NoteType
 const JUDGMENT = preload(JUDGMENT_PATH).Judgment
 
+@export var lanes: Dictionary[int, Array] = {
+	NOTE_TYPE.UP: [],
+	NOTE_TYPE.LEFT: [],
+	NOTE_TYPE.RIGHT: [],
+	NOTE_TYPE.DOWN: [],
+	NOTE_TYPE.SPECIAL1: [],
+	NOTE_TYPE.SPECIAL2: [],
+}
+
 @onready var conductor: Conductor = %Conductor
 @onready var chart: Chart = conductor.chart
-@onready var debug_label = %DebugLabel
 @onready var judgment_label = %JudgmentLabel
-
 @onready var judgments = preload(JUDGMENT_PATH).new()
 
 
@@ -21,23 +31,38 @@ func _unhandled_input(event):
 		judge_input(NOTE_TYPE.RIGHT)
 	if event.is_action_pressed("down"):
 		judge_input(NOTE_TYPE.DOWN)
+	if event.is_action_pressed("primary_action"):
+		judge_input(NOTE_TYPE.SPECIAL1)
+	if event.is_action_pressed("secondary_action"):
+		judge_input(NOTE_TYPE.SPECIAL2)
+
+
+func _ready():
+	lanes = chart.lanes.duplicate(true)
 
 
 func _process(_delta):
-	#var note: NoteData = chart.get_nearest_note(conductor.song_position)
-	var note: NoteData = chart.get_nearest_note_by_type(conductor.song_position, NOTE_TYPE.UP)
+	_check_for_missed_notes()
 
-	if note == null:
-		return
-	var time = note.cached_absolute_beat - conductor.song_position
 
-	debug_label.text = "%s %s %s" % [conductor.song_position, time, abs(time)]
+func _check_for_missed_notes():
+	for lane in lanes.keys():
+		var notes = lanes[lane]
+		if notes.is_empty():
+			continue
+
+		var note: NoteData = notes[0]
+
+		if conductor.song_position - note.cached_absolute_beat > judgments.miss:
+			note_missed.emit(note)
+			notes.pop_front()
 
 
 func judge_input(note_type: NOTE_TYPE):
-	var note: NoteData = chart.get_nearest_note_by_type(conductor.song_position, note_type)
-	#var note: NoteData = chart.get_nearest_note(conductor.song_position)
-
+	var lane = lanes[note_type]
+	if lane == null:
+		return
+	var note: NoteData = lane.front()
 	if note == null:
 		judgment_label.text = "Miss"
 		%Early_Late.text = ""
@@ -47,9 +72,15 @@ func judge_input(note_type: NOTE_TYPE):
 	if absolute_time <= judgments.perfect:
 		judgment_label.text = "Perfect"
 		%Early_Late.text = ""
+		note_played.emit(note, JUDGMENT.PERFECT)
+		lane.pop_front()
 	elif absolute_time <= judgments.great:
 		judgment_label.text = "Great"
 		%Early_Late.text = "Early" if time > 0 else "Late"
+		note_played.emit(note, JUDGMENT.GREAT)
+		lane.pop_front()
 	elif absolute_time <= judgments.okay:
 		judgment_label.text = "Okay"
 		%Early_Late.text = "Early" if time > 0 else "Late"
+		note_played.emit(note, JUDGMENT.OKAY)
+		lane.pop_front()
