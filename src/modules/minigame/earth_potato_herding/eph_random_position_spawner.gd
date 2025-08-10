f6769e11a5894a2e9d52627040e98cedc05055e4
MousePotatoDoesStuff <mousepotato@tutanota.com>
2025-08-10 13:05:50 +0200

    Uh... in what order are functions supposed to be?

--- src/modules/minigame/earth_potato_herding/eph_random_position_spawner.gd
+++ src/modules/minigame/earth_potato_herding/eph_random_position_spawner.gd
@@ -2,17 +2,17 @@ class_name EPHRandomPositionSpawner
 extends RandomPositionSpawner
 
 # Points that define spawn rate over time. X value should be non-descending.
-@export var points: Array[Vector2] = [Vector2.ZERO, Vector2.ONE] 
+@export var points: Array[Vector2] = [Vector2.ZERO, Vector2.ONE]
 @export var cur_mode: OvertimeMode = OvertimeMode.FINAL_RATE
 
-@export var time_multiplier: float = 20.0  
-@export var autospawn: bool = false 
+@export var time_multiplier: float = 20.0
+@export var autospawn: bool = false
 # I couldn't get Path2D or Curve to work the way they should,
 # so here's an array of points for now.
 
 # How spawn rate behaves when last point is passed
-enum OvertimeMode { ONESHOT = 0, REPEAT = 1, FINAL_RATE = 2, BEGIN_RATE = 3 }
 var _total_delta_time: float = 0
+enum OvertimeMode { ONESHOT = 0, REPEAT = 1, FINAL_RATE = 2, BEGIN_RATE = 3 }
 
 
 func _set_up() -> void:
@@ -20,6 +20,29 @@ func _set_up() -> void:
 	_total_delta_time = 0.0
 	if not autospawn:
 		_time_since_last_spawn = _spawn_cooldown
+
+
+#region ======================== PRIVATE METHODS ===============================
+
+
+func _physics_process(delta: float) -> void:
+	_total_delta_time += delta
+	if _use_physics_process_to_spawn:
+		var multiplier := get_current_rate()
+		_time_since_last_spawn -= delta * multiplier
+		_try_spawn()
+
+
+func _try_spawn() -> void:
+	if _time_since_last_spawn > 0:
+		return
+
+	_time_since_last_spawn = _spawn_cooldown
+
+	spawn()
+
+
+#endregion
 
 
 func get_current_rate() -> float:
@@ -54,25 +77,3 @@ func get_current_rate() -> float:
 	# Hey, as long as it works...
 	# print([_total_delta_time,cur_time,time_multiplier,cur_time,res])
 	return res.y
-
-
-#region ======================== PRIVATE METHODS ===============================
-
-
-func _physics_process(delta: float) -> void:
-	_total_delta_time += delta
-	if _use_physics_process_to_spawn:
-		var multiplier := get_current_rate()
-		_time_since_last_spawn -= delta * multiplier
-		_try_spawn()
-
-
-func _try_spawn() -> void:
-	if _time_since_last_spawn > 0:
-		return
-
-	_time_since_last_spawn = _spawn_cooldown
-
-	spawn()
-
-#endregion
