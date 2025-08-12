040747da135b019dbf080435f3aeb4c2663fa957
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-12 22:24:47 +0100

    Remove tween free

--- src/modules/main/splash_utils.gd
+++ src/modules/main/splash_utils.gd
@@ -1,14 +1,6 @@
 class_name SplashUtils
 extends Control
 
-var _current_tween: Tween
-
-
-func _exit_tree() -> void:
-	if is_instance_valid(_current_tween):
-		_current_tween.free.call_deferred()
-		return
-
 
 func fade_in_scene(current_scene: Node, fade_time: float = 1.0) -> void:
 	current_scene.modulate.a = 0.0  # invisible at start
@@ -20,9 +12,9 @@ func fade_in_scene(current_scene: Node, fade_time: float = 1.0) -> void:
 		current_scene.queue_free()
 		return
 
-	_current_tween = current_scene.create_tween()
-	_current_tween.tween_property(current_scene, "modulate:a", 1.0, fade_time)
-	await _current_tween.finished
+	var tween = current_scene.create_tween()
+	tween.tween_property(current_scene, "modulate:a", 1.0, fade_time)
+	await tween.finished
 
 
 func fade_out_scene(current_scene: Node, fade_time: float = 1.0) -> void:
@@ -34,6 +26,6 @@ func fade_out_scene(current_scene: Node, fade_time: float = 1.0) -> void:
 		current_scene.queue_free()
 		return
 
-	_current_tween = current_scene.create_tween()
-	_current_tween.tween_property(current_scene, "modulate:a", 0.0, fade_time)
-	await _current_tween.finished
+	var tween = current_scene.create_tween()
+	tween.tween_property(current_scene, "modulate:a", 0.0, fade_time)
+	await tween.finished
