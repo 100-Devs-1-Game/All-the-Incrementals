a735b97e5e7dce1e516e33468ad5ca5a7e862830
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-12 15:55:46 -0400

    Add collision to river bank
    Adds collision to the river bank

--- src/modules/minigame/water_rowing_rapids/water_rowing_rapids.gd
+++ src/modules/minigame/water_rowing_rapids/water_rowing_rapids.gd
@@ -4,9 +4,10 @@ extends BaseMinigame
 
 func _initialize():
 	var river_polygon: PackedVector2Array = $River.curve.get_baked_points()
-	var water: Polygon2D = $Wadder
+	var water: Polygon2D = %Wadder
 	water.polygon = river_polygon
-	water.texture_scale = Vector2.ONE * 50.0 / 1000.0
+	%RiverCollider.shape.segments = polyline_to_segments(river_polygon)
+	water.texture_scale = Vector2.ONE * 50.0 / 1000.0  # don't ask
 
 
 func _start():
@@ -15,3 +16,12 @@ func _start():
 
 func _on_rowing_player_spirit_collected(value: int) -> void:
 	add_score(value)
+
+
+func polyline_to_segments(polyline: PackedVector2Array) -> PackedVector2Array:
+	var constructed: PackedVector2Array = []
+	constructed.resize(len(polyline) * 2 - 2)
+	for i in len(polyline) - 1:
+		constructed[i * 2] = polyline[i]
+		constructed[i * 2 + 1] = polyline[i + 1]
+	return constructed
