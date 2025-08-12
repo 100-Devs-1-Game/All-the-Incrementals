57f7c2108324fed26dac3430d46e3cb48de5ec16
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-12 15:42:11 -0400

    Change Rowing Rapids River to be path-based
    updates Rowing Rapids to a suboptimal (it works) path-based definition method

--- src/modules/minigame/water_rowing_rapids/water_rowing_rapids.gd
+++ src/modules/minigame/water_rowing_rapids/water_rowing_rapids.gd
@@ -2,8 +2,11 @@ class_name WaterRowingRapidsMinigame
 extends BaseMinigame
 
 
-func _init() -> void:
-	pass
+func _initialize():
+	var river_polygon: PackedVector2Array = $River.curve.get_baked_points()
+	var water: Polygon2D = $Wadder
+	water.polygon = river_polygon
+	water.texture_scale = Vector2.ONE * 50.0 / 1000.0
 
 
 func _start():
