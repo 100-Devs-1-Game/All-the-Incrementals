571d27ecd5cc685531c993f9f1598654a352792a
Plant <99131806+PlantDev@users.noreply.github.com>
2025-08-13 20:29:41 -0400

    gtlint

--- src/modules/minigame/earth_tower_tumble/player.gd
+++ src/modules/minigame/earth_tower_tumble/player.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 @export var potato: PackedScene
-@export var shoot_delay: = 0.3
-@export var transition_speed: = 3020.0
-@export var direction_speed: = 420.0
+@export var shoot_delay := 0.3
+@export var transition_speed := 3020.0
+@export var direction_speed := 420.0
 @export var follow: PathFollow2D
 
 var can_shoot := true
 var start_position: Vector2
 @onready var inst = get_tree().current_scene
+
 
 func _ready():
 	start_position = global_position
@@ -16,6 +17,7 @@ func _ready():
 		var path2d := inst.find_child("Path2D")
 		if path2d and path2d.get_child_count() > 0:
 			follow = path2d.get_child(0)
+
 
 func _input(event: InputEvent) -> void:
 	if not inst.build_mode and event.is_action_pressed("primary_action"):
@@ -25,6 +27,7 @@ func _input(event: InputEvent) -> void:
 		inst.build_mode = not inst.build_mode
 		if follow:
 			follow.progress_ratio = 0.5
+
 
 func _shoot():
 	if not can_shoot or potato == null:
@@ -36,15 +39,22 @@ func _shoot():
 	await get_tree().create_timer(shoot_delay).timeout
 	can_shoot = true
 
+
 func _process(delta: float) -> void:
 	if inst.build_mode:
 		global_position = global_position.move_toward(start_position, transition_speed * delta)
 	else:
 		if follow:
-			global_position = global_position.move_toward(follow.global_position, transition_speed * delta)
+			global_position = global_position.move_toward(
+				follow.global_position, transition_speed * delta
+			)
 
 	if not inst.build_mode and follow:
 		if Input.is_action_pressed("left"):
-			follow.progress_ratio = clamp(follow.progress_ratio - (direction_speed * 0.001) * delta, 0.0, 1.0)
+			follow.progress_ratio = clamp(
+				follow.progress_ratio - (direction_speed * 0.001) * delta, 0.0, 1.0
+			)
 		if Input.is_action_pressed("right"):
-			follow.progress_ratio = clamp(follow.progress_ratio + (direction_speed * 0.001) * delta, 0.0, 1.0)
+			follow.progress_ratio = clamp(
+				follow.progress_ratio + (direction_speed * 0.001) * delta, 0.0, 1.0
+			)
