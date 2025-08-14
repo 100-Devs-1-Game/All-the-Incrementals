7b0b7e1b43ebfaa1b629c1d0ad8228d72acd3dc6
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-14 14:58:22 -0400

    Add Void rubberbanding
    Adds rubberbanding to the void, making it faster the further away from the player it is.

--- src/modules/minigame/water_rowing_rapids/void/void.gd
+++ src/modules/minigame/water_rowing_rapids/void/void.gd
@@ -1,24 +1,32 @@
 extends Node2D
 
 @export var volume_fadeout: Curve
+@export var rubberband: Curve
 
 var player: Node2D
 
-var speed: float = 300.0
+var speed: float = 700.0
+var accel: float = 50.0
 
 @onready var whispers: AudioStreamPlayer = $Whispers
 
 
 func _ready() -> void:
 	volume_fadeout.bake()
+	rubberband.bake()
 
 
 func _process(_delta: float) -> void:
-	var distance := maxf(player.position.x, position.x) - position.x
+	var distance := get_distance()
 	var fade := volume_fadeout.sample_baked(distance)
 	Music._music_player.volume_linear = fade
 	whispers.volume_linear = 1.0 - fade
 
 
 func _physics_process(delta: float) -> void:
-	position.x += speed * delta
+	var rubberbanded_speed = rubberband.sample_baked(get_distance()) * speed
+	position.x += rubberbanded_speed * delta
+
+
+func get_distance() -> float:
+	return maxf(player.position.x, position.x) - position.x
