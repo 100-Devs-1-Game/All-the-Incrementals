eff76187319cf2e496c2ea004509232217690c44
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-16 02:59:51 +0200

    Add barrel break sound

--- src/modules/minigame/fire_fighters/items/fire_fighter_oil_barrel.gd
+++ src/modules/minigame/fire_fighters/items/fire_fighter_oil_barrel.gd
@@ -2,6 +2,7 @@ class_name FireFightersMinigameOilBarrel
 extends FireFightersMinigameItem
 
 @export var spill_radius: int = 4
+@export var break_sound: AudioStream
 
 
 func _on_burn(game: FireFightersMinigame, item: FireFightersMinigameItemInstance):
@@ -11,4 +12,5 @@ func _on_burn(game: FireFightersMinigame, item: FireFightersMinigameItemInstance
 
 func _on_destroy(game: FireFightersMinigame, item: FireFightersMinigameItemInstance):
 	game.oil_explosion(game.get_tile_at(item.position), spill_radius, false)
+	game.play_audio_effect(break_sound, item.position)
 	item.queue_free()
