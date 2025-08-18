1c51d6ea8c0c61ac916740af7e5968f6df94cc88
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-18 00:43:39 -0400

    Rock break dust

--- src/modules/minigame/water_rowing_rapids/rock/rock.gd
+++ src/modules/minigame/water_rowing_rapids/rock/rock.gd
@@ -4,6 +4,7 @@ extends StaticBody2D
 func explode() -> void:
 	var explosion_particles: GPUParticles2D = $ExplosionParticles
 	explosion_particles.emitting = true
+	$DustParticles.emitting = true
 	$Sprite2D.visible = false
 	process_mode = Node.PROCESS_MODE_DISABLED
 	await get_tree().create_timer(explosion_particles.lifetime).timeout
