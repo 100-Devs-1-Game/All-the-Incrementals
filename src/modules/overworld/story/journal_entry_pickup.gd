9be3d053cd10f894335847e1bffc17374503c77b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-18 03:39:19 +0200

    Refactor

--- src/modules/overworld/story/journal_entry_pickup.gd
+++ src/modules/overworld/story/journal_entry_pickup.gd
@@ -1,4 +1,9 @@
 class_name JournalEntryPickup
-extends Area3D
+extends Node3D
 
 @export var data: JournalEntryData
+
+
+func _on_interaction_component_interacted_with(_player: SpiritkeeperCharacterController3D) -> void:
+	EventBus.request_journal_page_display.emit(data)
+	queue_free()
