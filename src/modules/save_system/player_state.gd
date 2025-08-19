c33585b23883c5efd9007e3c95e7950b66363913
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-20 01:19:16 +0200

    Update comment

--- src/modules/save_system/player_state.gd
+++ src/modules/save_system/player_state.gd
@@ -3,5 +3,5 @@ extends Resource
 
 @export var inventory: EssenceInventory
 
-# Dictionary mapping minigame UIDs to an array of their latest 5 scores
+# Dictionary mapping MinigameData resource path to an array of their latest 5 scores
 @export var highscores: Dictionary[StringName, Array]
