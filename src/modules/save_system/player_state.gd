085a2d0abd45b332b6f44089fcb0a4068c038b98
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-20 06:16:22 +0200

    Add get_average_minigame_highscore()

--- src/modules/save_system/player_state.gd
+++ src/modules/save_system/player_state.gd
@@ -5,3 +5,23 @@ extends Resource
 
 # Dictionary mapping MinigameData resource path to an array of their latest 5 scores
 @export var highscores: Dictionary[StringName, Array]
+
+
+func get_average_minigame_highscore(minigame: MinigameData) -> float:
+	var key: StringName = minigame.resource_path
+
+	if not highscores.has(key):
+		return 0
+
+	var all_highscores: Array[int] = highscores[key]
+
+	if all_highscores.is_empty():
+		return 0
+
+	var highscore_sum: int
+	for highscore in all_highscores:
+		highscore_sum += highscore
+
+	var avg_highscore: float = highscore_sum / float(all_highscores.size())
+
+	return avg_highscore
