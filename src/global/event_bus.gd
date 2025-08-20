4c2d0b86fdb26fb1b348a6a751ed219c2c486d0b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-20 18:37:45 +0200

    Add passive_essence_generation_tick signal

--- src/global/event_bus.gd
+++ src/global/event_bus.gd
@@ -12,6 +12,7 @@ signal request_journal_page_display(data: JournalEntryData)
 #region Game
 signal game_loaded(world_state: WorldState)
 signal exit_minigame
+signal passive_essence_generation_tick
 #endregion
 
 #region UI
