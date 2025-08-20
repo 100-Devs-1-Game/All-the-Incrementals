31d8b131d981c7e45f951f9b7665a1af0ace0c2c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-20 02:32:21 +0200

    Update altar_upgrade_logic.gd

--- src/modules/overworld/altar/altar_upgrade_logic.gd
+++ src/modules/overworld/altar/altar_upgrade_logic.gd
@@ -1,25 +1,14 @@
 class_name AltarUpgradeLogic
 extends BaseMinigameUpgradeLogic
 
-enum Altar { EARTH, FIRE, WATER, WIND }
 enum Type { CAPACITY, THROUGHPUT }
 
-@export var essence: Altar
+@export var essence: Altar.Element
 @export var type: Type
 
 
 func _apply_effect(_game: BaseMinigame, upgrade: MinigameUpgrade):
-	var altar: AltarStats
-
-	match essence:
-		Altar.EARTH:
-			altar = SaveGameManager.world_state.earth_altar
-		Altar.FIRE:
-			altar = SaveGameManager.world_state.fire_altar
-		Altar.WATER:
-			altar = SaveGameManager.world_state.water_altar
-		Altar.WIND:
-			altar = SaveGameManager.world_state.wind_altar
+	var altar: AltarStats = SaveGameManager.world_state.get_altar(essence)
 
 	match type:
 		Type.CAPACITY:
