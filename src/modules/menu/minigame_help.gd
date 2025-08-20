78ae626a090fa755531b9e8ead3a3481339897f6
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-20 16:40:49 -0400

    Add keybind autofill in minigame help

--- src/modules/menu/minigame_help.gd
+++ src/modules/menu/minigame_help.gd
@@ -18,8 +18,13 @@ func setup(p_minigame: BaseMinigame) -> void:
 	minigame = p_minigame
 	minigame.playing.connect(
 		func():
+			var binds: Dictionary[String, String] = {}
+			for k in GameSettings.keybinds:
+				if (GameSettings.keybinds[k] as Array).is_empty():
+					continue
+				binds[k] = OS.get_keycode_string(GameSettings.keybinds[k][0])
 			help_text.text = ""
-			help_text.append_text(minigame.data.how_to_play)
+			help_text.append_text(minigame.data.how_to_play.format(binds))
 	)
 
 
