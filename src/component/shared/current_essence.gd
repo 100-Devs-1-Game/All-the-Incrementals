797eece24093999f418e324899f7f36c859d5e12
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-20 02:34:54 +0200

    Update current_essence.gd

--- src/component/shared/current_essence.gd
+++ src/component/shared/current_essence.gd
@@ -3,11 +3,11 @@ extends Control
 
 
 func _ready() -> void:
-	EventBus.ui_upgrade_bought.connect(_refresh_labels)
-	_refresh_labels()
+	EventBus.ui_upgrade_bought.connect(refresh_labels)
+	refresh_labels()
 
 
-func _refresh_labels(_upgrade = null) -> void:
+func refresh_labels(_upgrade = null) -> void:
 	var inventory = Player.data.inventory
 	$Panel/HBoxContainer/WaterLabel.text = "%4d" % inventory.water
 	$Panel/HBoxContainer/EarthLabel.text = "%4d" % inventory.earth
