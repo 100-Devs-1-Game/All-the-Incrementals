d070e9fe344817fcee31e2caf9961e35f1a12931
SaronTetra <starwars.maciek@gmail.com>
2025-08-22 01:32:58 +0200

    Wind Rhythm | Change judgment line to note markers, change note display

--- src/modules/minigame/wind_rhythm/note/note.gd
+++ src/modules/minigame/wind_rhythm/note/note.gd
@@ -1,28 +1,54 @@
 extends Node2D
 
 const JUDGMENT = preload("res://modules/minigame/wind_rhythm/chart/judgments.gd").Judgment
+const NOTE_TYPE = preload("res://modules/minigame/wind_rhythm/chart/note_types.gd").NoteType
 
 @export var speed = 500
 var note_time: float = 0
 var type: int
+
+var arrow_sprite: Dictionary[NOTE_TYPE, CompressedTexture2D] = {
+	NOTE_TYPE.UP: preload("res://assets/minigames/wind_rhythm/notes/up_arrow.png"),
+	NOTE_TYPE.LEFT: preload("res://assets/minigames/wind_rhythm/notes/left_arrow.png"),
+	NOTE_TYPE.RIGHT: preload("res://assets/minigames/wind_rhythm/notes/right_arrow.png"),
+	NOTE_TYPE.DOWN: preload("res://assets/minigames/wind_rhythm/notes/down_arrow.png"),
+	NOTE_TYPE.SPECIAL1: preload("res://assets/minigames/wind_rhythm/notes/primary_icon.png"),
+	NOTE_TYPE.SPECIAL2: preload("res://assets/minigames/wind_rhythm/notes/secondary_icon.png"),
+}
+
+var judgment_sprite: Dictionary[JUDGMENT, CompressedTexture2D] = {
+	JUDGMENT.PERFECT: preload("res://assets/minigames/wind_rhythm/notes/perfect_variant.png"),
+	JUDGMENT.GREAT: preload("res://assets/minigames/wind_rhythm/notes/great_variant.png"),
+	JUDGMENT.OKAY: preload("res://assets/minigames/wind_rhythm/notes/okay_variant.png"),
+	JUDGMENT.MISS: preload("res://assets/minigames/wind_rhythm/notes/miss_variant.png"),
+}
+
+var marker_sprite: Dictionary[NOTE_TYPE, CompressedTexture2D] = {
+	NOTE_TYPE.UP: preload("res://assets/minigames/wind_rhythm/notes/up_note_marker.png"),
+	NOTE_TYPE.LEFT: preload("res://assets/minigames/wind_rhythm/notes/left_note_marker.png"),
+	NOTE_TYPE.RIGHT: preload("res://assets/minigames/wind_rhythm/notes/right_note_marker.png"),
+	NOTE_TYPE.DOWN: preload("res://assets/minigames/wind_rhythm/notes/down_note_marker.png"),
+	#NOTE_TYPE.SPECIAL1: preload("res://assets/minigames/wind_rhythm/notes/primary_icon.png"),
+	#NOTE_TYPE.SPECIAL2: preload("res://assets/minigames/wind_rhythm/notes/secondary_icon.png"),
+}
 
 
 func _process(delta):
 	position.x -= delta * speed
 
 
+func _ready():
+	%Arrow.texture = arrow_sprite[type]
+	%ArrowFill.texture = marker_sprite.get(type)
+
+
 func on_miss(note: NoteData):
 	if note.type == type:
 		if note_time == note.cached_absolute_beat:
-			%Background.self_modulate = Color(1, 0, 0)
+			%Judgment.texture = judgment_sprite[JUDGMENT.MISS]
 
 
 func on_hit(note: NoteData, judgment: JUDGMENT):
 	if note.type == type:
 		if note_time == note.cached_absolute_beat:
-			if judgment & JUDGMENT.PERFECT:
-				%Background.self_modulate = Color(0, 1, 0)
-			if judgment & JUDGMENT.GREAT:
-				%Background.self_modulate = Color(0.5, 1, 0)
-			if judgment & JUDGMENT.OKAY:
-				%Background.self_modulate = Color(1, 1, 0)
+			%Judgment.texture = judgment_sprite[judgment]
--- src/modules/minigame/wind_rhythm/note/note_spawner.gd
+++ src/modules/minigame/wind_rhythm/note/note_spawner.gd
@@ -14,15 +14,31 @@ var chart: Chart
 var conductor: Conductor
 var manager: RhythmGame
 
+var arrow_sprite: Dictionary[NOTE_TYPE, CompressedTexture2D] = {
+	NOTE_TYPE.UP: preload("res://assets/minigames/wind_rhythm/notes/markers/up_arrow_white.png"),
+	NOTE_TYPE.LEFT:
+	preload("res://assets/minigames/wind_rhythm/notes/markers/left_arrow_white.png"),
+	NOTE_TYPE.RIGHT:
+	preload("res://assets/minigames/wind_rhythm/notes/markers/right_arrow_white.png"),
+	NOTE_TYPE.DOWN:
+	preload("res://assets/minigames/wind_rhythm/notes/markers/down_arrow_white.png"),
+	NOTE_TYPE.SPECIAL1:
+	preload("res://assets/minigames/wind_rhythm/notes/markers/primary_icon_white.png"),
+	NOTE_TYPE.SPECIAL2:
+	preload("res://assets/minigames/wind_rhythm/notes/markers/secondary_icon_white.png"),
+}
+
 
 func _ready():
 	conductor = get_node(conductor_path)
 	chart = conductor.chart
 	manager = get_node(wind_rhythm)
+	spawn_markers()
 	spawn_notes()
 
 
 func spawn_notes():
+	print("offset: %s" % offset)
 	for lane in chart.lanes.keys():
 		for note in chart.lanes[lane]:
 			var absolute_beat = note.bar * conductor.notes_in_bar + note.beat + offset
@@ -32,29 +48,52 @@ func spawn_notes():
 
 			# TODO: Remove hardcoded lanes positions
 			if note.type & NOTE_TYPE.UP:
-				spawn_note(spawn_x, 0, 30, note.copy({"type": NOTE_TYPE.UP}))
+				spawn_note(spawn_x, 30, note.copy({"type": NOTE_TYPE.UP}))
 			if note.type & NOTE_TYPE.LEFT:
-				spawn_note(spawn_x, -PI / 2, 77, note.copy({"type": NOTE_TYPE.LEFT}))
+				spawn_note(spawn_x, 77, note.copy({"type": NOTE_TYPE.LEFT}))
 			if note.type & NOTE_TYPE.RIGHT:
-				spawn_note(spawn_x, PI / 2, 125, note.copy({"type": NOTE_TYPE.RIGHT}))
+				spawn_note(spawn_x, 125, note.copy({"type": NOTE_TYPE.RIGHT}))
 			if note.type & NOTE_TYPE.DOWN:
-				spawn_note(spawn_x, PI, 175, note.copy({"type": NOTE_TYPE.DOWN}))
+				spawn_note(spawn_x, 175, note.copy({"type": NOTE_TYPE.DOWN}))
 			if note.type & NOTE_TYPE.SPECIAL1:
-				spawn_note(spawn_x, PI / 4, 220, note.copy({"type": NOTE_TYPE.SPECIAL1}))
+				spawn_note(spawn_x, 220, note.copy({"type": NOTE_TYPE.SPECIAL1}))
 			if note.type & NOTE_TYPE.SPECIAL2:
-				spawn_note(spawn_x, -PI / 4, 270, note.copy({"type": NOTE_TYPE.DOWN}))
+				spawn_note(spawn_x, 270, note.copy({"type": NOTE_TYPE.SPECIAL2}))
 
 
-# TODO: Use different arrow sprites instead of rotating
-func spawn_note(new_x_position: float, new_rotation: float, lane_position: int, note: NoteData):
+func spawn_note(new_x_position: float, lane_position: int, note: NoteData):
 	var judgment_x = get_node(judgment_line).global_position.x
 	var note_instance = NOTE.instantiate()
-	note_instance.global_position = Vector2(judgment_x + new_x_position, lane_position)
+	note_instance.global_position = Vector2(judgment_x + new_x_position, lane_position * 2)
 	note_instance.speed = judgment_x / conductor.seconds_per_beat * scroll_speed
 
-	note_instance.rotate(new_rotation)
 	note_instance.note_time = note.cached_absolute_beat
 	note_instance.type = note.type
 	manager.note_missed.connect(note_instance.on_miss)
 	manager.note_played.connect(note_instance.on_hit)
 	add_child(note_instance)
+
+
+func spawn_markers():
+	for lane in [
+		[30, NOTE_TYPE.UP],
+		[77, NOTE_TYPE.LEFT],
+		[125, NOTE_TYPE.RIGHT],
+		[175, NOTE_TYPE.DOWN],
+		[220, NOTE_TYPE.SPECIAL1],
+		[270, NOTE_TYPE.SPECIAL2]
+	]:
+		var judgment_x = get_node(judgment_line).global_position.x
+		var marker = Sprite2D.new()
+		marker.texture = arrow_sprite[lane[1]]
+		marker.position = Vector2(judgment_x, lane[0] * 2)
+		marker.scale = Vector2(0.35, 0.35)
+		add_child(marker)
+
+		var marker_outline = Sprite2D.new()
+		marker_outline.texture = preload(
+			"res://assets/minigames/wind_rhythm/notes/arrow_score_background.png"
+		)
+		marker_outline.position = Vector2(judgment_x, lane[0] * 2)
+		marker_outline.scale = Vector2(0.35, 0.35)
+		add_child(marker_outline)
