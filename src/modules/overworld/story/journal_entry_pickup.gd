c9ee8cf1881f024db6015f641b571355bcbe32a7
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-21 16:04:14 -0400

    update page texture based on journal data

--- src/modules/overworld/story/journal_entry_pickup.gd
+++ src/modules/overworld/story/journal_entry_pickup.gd
@@ -3,7 +3,15 @@ extends Node3D
 
 @export var data: JournalEntryData
 
+@onready var page: MeshInstance3D = $HoveringPage
+
 
 func _on_interaction_component_interacted_with(_player: SpiritkeeperCharacterController3D) -> void:
 	EventBus.request_journal_page_display.emit(data)
 	queue_free()
+
+
+func _ready() -> void:
+	((page.mesh as QuadMesh).material as ShaderMaterial).set_shader_parameter(
+		&"texture_albedo", data.page
+	)
