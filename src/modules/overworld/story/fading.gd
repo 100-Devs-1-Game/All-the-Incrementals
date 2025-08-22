7d205dd1eaf25d87884ecaa6f595aae535197a11
UwUMacaroniTime <102434693+UwUMacaroniTime@users.noreply.github.com>
2025-08-22 15:59:01 -0400

    Implement custom smoothed scrollling Container

--- src/modules/overworld/story/fading.gd
+++ src/modules/overworld/story/fading.gd
@@ -1,5 +1,7 @@
 @tool
-extends ScrollContainer
+extends Container
+
+const DRAW_TOLERANCE: float = 8
 
 @export var gradient: GradientTexture2D:
 	set(new):
@@ -10,24 +12,79 @@ extends ScrollContainer
 		if is_instance_valid(gradient):
 			gradient.changed.connect(queue_redraw)
 
+@export var scroll_value: float:
+	set(new):
+		scroll_value = clamp_scroll(new)
+		_update_children_position()
+
+var scroll_target_value: float = scroll_value:
+	set(new):
+		scroll_target_value = clamp_scroll(new)
+var content_max: float
+var scroll_max: float
+
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	if is_equal_approx(scroll_value, scroll_target_value):
+		return
+	scroll_value = lerpf(scroll_value, scroll_target_value, delta * 8)
+
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var modified: bool = false
+
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			scroll_target_value -= (content_max - size.y) / 8 * event.factor
+			modified = true
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			scroll_target_value += (content_max - size.y) / 8 * event.factor
+			modified = true
+
+		if modified:
+			accept_event()
+			return
+
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_SORT_CHILDREN:
+			for c in get_children():
+				if c is not Control:
+					return
+				var minsize := (c as Control).get_combined_minimum_size()
+				var rect: Rect2 = Rect2(Vector2.ZERO, minsize)
+				if c.size_flags_horizontal && SIZE_EXPAND:
+					rect.size.x = max(size.x, minsize.x)
+				if c.size_flags_vertical && SIZE_EXPAND:
+					rect.size.y = max(size.y, minsize.y)
+				fit_child_in_rect(c, rect)
+				content_max = maxf(scroll_max, rect.size.y)
+			scroll_max = content_max - size.y
+			_update_children_position()
+
 
 func _draw() -> void:
-	print("draw")
 	if not gradient:
 		return
 	var gradient_height: int = gradient.get_height()
-	var scroll: VScrollBar = get_v_scroll_bar()
 	var unreserved_space: Vector2 = Vector2(0, size.y)
-	if scroll.value > 0:
+
+	if scroll_value - DRAW_TOLERANCE > 0:
 		draw_texture_rect(gradient, Rect2(Vector2.ZERO, Vector2(size.x, gradient_height)), false)
 		unreserved_space[0] += gradient_height
-	if (scroll.value + scroll.page) < scroll.max_value:
+
+	if scroll_value + DRAW_TOLERANCE < scroll_max:
 		draw_set_transform(Vector2.ZERO, 0, Vector2(1, -1))
 		draw_texture_rect(
 			gradient, Rect2(Vector2(0, -size.y), Vector2(size.x, gradient_height)), false
 		)
 		unreserved_space[1] -= gradient_height
+
 	draw_set_transform(Vector2.ZERO)
+
 	draw_rect(
 		Rect2(
 			Vector2(0, unreserved_space[0]),
@@ -35,3 +92,15 @@ func _draw() -> void:
 		),
 		Color.WHITE
 	)
+
+
+func _update_children_position() -> void:
+	for c in get_children():
+		if c is not Control:
+			return
+		c.position.y = -scroll_value
+	queue_redraw()
+
+
+func clamp_scroll(value: float) -> float:
+	return clampf(value, 0, scroll_max)
