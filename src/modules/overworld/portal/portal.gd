da6d7a5171b24d13074bd727d849e788b17eb7a7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-23 03:28:21 +0200

    Fix portals

--- src/modules/overworld/portal/portal.gd
+++ src/modules/overworld/portal/portal.gd
@@ -6,10 +6,22 @@ extends Node3D
 
 
 func _ready() -> void:
-	interaction_component.action_ui_suffix = "enter " + target_settlement.display_name
+	var tmp_str: String
+	if target_settlement:
+		tmp_str = "enter " + target_settlement.display_name
+	else:
+		tmp_str = "return to Shrine"
+
+	interaction_component.action_ui_suffix = tmp_str
 
 
 func _on_interaction_component_3d_interacted_with(
 	_player: SpiritkeeperCharacterController3D
 ) -> void:
-	SceneLoader.enter_settlement.call_deferred(target_settlement)
+	SceneLoader.enter_settlement.call_deferred(
+		(
+			target_settlement
+			if target_settlement
+			else load("res://modules/overworld_locations/shrine/shrine.tres")
+		)
+	)
