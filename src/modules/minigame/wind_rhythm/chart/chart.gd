152dfd3e4e2fc618015c71241aa01a4ab9d01101
SaronTetra <starwars.maciek@gmail.com>
2025-08-24 16:38:52 +0200

    Wind Rhythm | Cleanup chart.gd

--- src/modules/minigame/wind_rhythm/chart/chart.gd
+++ src/modules/minigame/wind_rhythm/chart/chart.gd
@@ -7,10 +7,7 @@ const NOTE_TYPE = preload("res://modules/minigame/wind_rhythm/chart/note_types.g
 @export var name: String
 @export var audio: AudioStream
 @export var bpm: int
-@export_tool_button("Sort Bars", "Sort") var sort = sort_notes
-@export_tool_button("Calculate note times", "Time") var calculate = calculate_note_times
 @export_tool_button("Rebuild notes array", "Reload") var rebuild = rebuild_notes_array_from_lanes
-@export var notes: Array[NoteData]
 @export var notes_in_bar: int = 4
 @export var lanes: Dictionary[NOTE_TYPE, Array] = {
 	NOTE_TYPE.UP: [],
@@ -33,67 +30,13 @@ func _ready():
 	bpm = audio.bpm
 
 
-func _rebuild_notes_array():
-	calculate_note_times()
-	for lane in lanes.keys():
-		lanes[lane].clear()
-
-	for note: NoteData in notes:
-		if note.type & NOTE_TYPE.UP:
-			lanes[NOTE_TYPE.UP].append(note.copy({"type": NOTE_TYPE.UP}))
-		if note.type & NOTE_TYPE.LEFT:
-			lanes[NOTE_TYPE.LEFT].append(note.copy({"type": NOTE_TYPE.LEFT}))
-		if note.type & NOTE_TYPE.RIGHT:
-			lanes[NOTE_TYPE.RIGHT].append(note.copy({"type": NOTE_TYPE.RIGHT}))
-		if note.type & NOTE_TYPE.DOWN:
-			lanes[NOTE_TYPE.DOWN].append(note.copy({"type": NOTE_TYPE.DOWN}))
-		if note.type & NOTE_TYPE.SPECIAL1:
-			lanes[NOTE_TYPE.SPECIAL1].append(note.copy({"type": NOTE_TYPE.SPECIAL1}))
-		if note.type & NOTE_TYPE.SPECIAL2:
-			lanes[NOTE_TYPE.SPECIAL2].append(note.copy({"type": NOTE_TYPE.SPECIAL2}))
-
-	calculate_note_times_in_lanes()
-
-	for lane in lanes.keys():
-		lanes[lane].sort_custom(
-			func(a: NoteData, b: NoteData): return a.cached_absolute_beat < b.cached_absolute_beat
-		)
-
-
 func rebuild_notes_array_from_lanes():
-	notes.clear()
-
-	for lane in lanes.keys():
-		for note: NoteData in lanes[lane]:
-			if note.type & NOTE_TYPE.UP:
-				notes.append(note.copy({"type": NOTE_TYPE.UP}))
-			if note.type & NOTE_TYPE.LEFT:
-				notes.append(note.copy({"type": NOTE_TYPE.LEFT}))
-			if note.type & NOTE_TYPE.RIGHT:
-				notes.append(note.copy({"type": NOTE_TYPE.RIGHT}))
-			if note.type & NOTE_TYPE.DOWN:
-				notes.append(note.copy({"type": NOTE_TYPE.DOWN}))
-			if note.type & NOTE_TYPE.SPECIAL1:
-				notes.append(note.copy({"type": NOTE_TYPE.SPECIAL1}))
-			if note.type & NOTE_TYPE.SPECIAL2:
-				notes.append(note.copy({"type": NOTE_TYPE.SPECIAL2}))
-
 	calculate_note_times_in_lanes()
 
 	for lane in lanes.keys():
 		lanes[lane].sort_custom(
 			func(a: NoteData, b: NoteData): return a.cached_absolute_beat < b.cached_absolute_beat
 		)
-
-
-func sort_notes():
-	notes.sort_custom(func(a: NoteData, b: NoteData): return a.bar < b.bar)
-
-
-func calculate_note_times():
-	if notes != null:
-		for note: NoteData in notes:
-			note.cached_absolute_beat = (note.bar + note.beat / notes_in_bar) * (60.0 / audio.bpm)
 
 
 func calculate_note_times_in_lanes():
