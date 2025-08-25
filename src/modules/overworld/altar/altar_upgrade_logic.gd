3c69a2ce612925a39c3d0b8d7c64c14e6dcc15cf
Zephilinox <Zephilinox@hotmail.co.uk>
2025-08-25 21:33:37 +0100

    WIP Shrine Upgrades

--- src/modules/overworld/altar/altar_upgrade_logic.gd
+++ src/modules/overworld/altar/altar_upgrade_logic.gd
@@ -1,7 +1,12 @@
 class_name AltarUpgradeLogic
 extends BaseMinigameUpgradeLogic
 
-enum Type { CAPACITY, THROUGHPUT }
+enum Type {
+	CAPACITY_FLAT = 0,
+	CAPACITY_MULTIPLIER = 1,
+	THROUGHPUT_FLAT = 2,
+	THROUGHPUT_MULTIPLIER = 3,
+}
 
 @export var essence: Altar.Element
 @export var type: Type
@@ -11,7 +16,11 @@ func _apply_effect(_game: BaseMinigame, upgrade: MinigameUpgrade):
 	var altar: AltarStats = SaveGameManager.world_state.get_altar_stats(essence)
 
 	match type:
-		Type.CAPACITY:
-			altar.capacity = int(upgrade.get_current_effect_modifier())
-		Type.THROUGHPUT:
-			altar.throughput = int(upgrade.get_current_effect_modifier())
+		Type.CAPACITY_FLAT:
+			altar.capacity_flat += int(upgrade.get_current_effect_modifier())
+		Type.CAPACITY_MULTIPLIER:
+			altar.capacity_multiplier += upgrade.get_current_effect_modifier()
+		Type.THROUGHPUT_FLAT:
+			altar.throughput_flat = int(upgrade.get_current_effect_modifier())
+		Type.THROUGHPUT_MULTIPLIER:
+			altar.throughput_multiplier = upgrade.get_current_effect_modifier()
