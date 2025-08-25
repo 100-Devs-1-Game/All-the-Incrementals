ad67605e70138ba6ee57a13bbf4607d838dc6dbd
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-08-25 06:00:44 +0200

    Add how-to-play component

---
+++ src/modules/overworld_locations/shrine/how_to_play.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+var tween: Tween
+
+@onready var label_3d: Label3D = $Label3D
+
+
+func _ready() -> void:
+	start_bounce()
+
+
+func start_bounce():
+	tween = create_tween()
+	tween.tween_property(label_3d, "position:y", -0.3, 1)
+	tween.tween_property(label_3d, "position:y", 0.0, 1)
+	tween.set_loops()
+
+
+func stop_bounce():
+	if tween:
+		tween.kill()
+	label_3d.position.y = 0
+
+
+func _on_interaction_component_3d_interacted_with(
+	_player: SpiritkeeperCharacterController3D
+) -> void:
+	stop_bounce()
